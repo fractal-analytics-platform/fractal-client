e7a97dbd0b2a76cc9846880ee301081f5c86dce8
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-06-07 08:23:44 +0000

    Update compress_tif.py task, removing wrapper and batch parallelization [closes #65]

---
+++ fractal/tasks/compress_tif.py
@@ -0,0 +1,70 @@
+import glob
+import os
+
+from PIL import Image
+
+
+def compress_tif(in_path, out_path, delete_input=False):
+
+    """
+    Compress tiff files
+
+    :param in_path: directory containing the input files
+    :type in_path: str
+    :param out_path: directory containing the output files
+    :type out_path: str
+    :param delete_input: delete input files
+    :type delete_input: bool
+    """
+
+    # Sanitize input/output paths
+    if not in_path.endswith("/"):
+        in_path += "/"
+    if not out_path.endswith("/"):
+        out_path += "/"
+
+    # Create output path, if needed
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+
+    for filename in glob.glob(in_path + "*.tif"):
+        newfilename = os.path.join(out_path, os.path.basename(filename))
+
+        # Save compressed image
+        with Image.open(filename) as image:
+            image.save(newfilename, format="tiff", compression="tiff_lzw")
+        print(f"Raw:        {filename}\nCompressed: {newfilename}")
+
+        # Delete raw image, if needed
+        if delete_input:
+            try:
+                os.remove(filename)
+                print(f"Deleted:    {filename}")
+            except OSError as e:
+                print("ERROR: %s : %s" % (filename, e.strerror))
+
+        print()
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(prog="compress_tif")
+    parser.add_argument("in_path", help="directory containing the input files")
+    parser.add_argument(
+        "out_path", help="directory containing the output files"
+    )
+    parser.add_argument(
+        "-d",
+        "--delete-input",
+        action="store_true",
+        help="Delete input files",
+    )
+
+    args = parser.parse_args()
+
+    compress_tif(
+        args.in_path,
+        args.out_path,
+        delete_input=args.delete_input,
+    )
