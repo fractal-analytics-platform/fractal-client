ce6519142e04ae3a102ad0719896e7aa957a87dd
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-06-27 22:28:22 +0200

    conform to new fixture

--- tests/server/test_alive.py
+++ tests/server/test_alive.py
@@ -1,5 +1,5 @@
 async def test_alive(client):
-    res = await client.get("/alive/")
+    res = await client.get("/api/alive/")
     data = res.json()
     assert res.status_code == 200
     assert data["alive"] is True
