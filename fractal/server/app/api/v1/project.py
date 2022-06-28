476b9e5276408fe4915b84cc1dbf2dbd3c7585ca
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-06-28 09:04:12 +0200

    project api stub

--- fractal/server/app/api/v1/project.py
+++ fractal/server/app/api/v1/project.py
@@ -1,5 +1,8 @@
+from typing import List
+
 from fastapi import APIRouter
 from fastapi import Depends
+from sqlalchemy.future import select
 
 from ...db import AsyncSession
 from ...db import get_db
@@ -11,18 +14,23 @@ from ...security import User
 router = APIRouter()
 
 
+@router.get("/", response_model=List[Project])
+async def get_list_project(
+    db: AsyncSession = Depends(get_db),
+):
+    print("I'm here")
+    result = await db.execute(select(Project))
+    return result.scalars().all()
+
+
 @router.post("/", response_model=Project, status_code=201)
 async def create_project(
     project: Project,
     user: User = Depends(current_active_user),
     db: AsyncSession = Depends(get_db),
 ):
-    from devtools import debug
-
-    debug(user)
-    debug(project)
-
+    project = Project(name="project", project_dir="/tpm/")
     db.add(project)
     await db.commit()
-
+    await db.refresh(project)
     return project
