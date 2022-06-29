9fe3b0fbed5c6215925e80ce60d6bd0a10430dc8
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-06-29 17:54:29 +0200

    implement project post, get list and models

--- fractal/server/app/api/v1/project.py
+++ fractal/server/app/api/v1/project.py
@@ -7,6 +7,8 @@ from sqlalchemy.future import select
 from ...db import AsyncSession
 from ...db import get_db
 from ...models.project import Project
+from ...models.project import ProjectCreate
+from ...models.project import ProjectRead
 from ...security import current_active_user
 from ...security import User
 
@@ -14,29 +16,31 @@ from ...security import User
 router = APIRouter()
 
 
-@router.get("/", response_model=List[Project])
+@router.get("/", response_model=List[ProjectRead])
 async def get_list_project(
+    user: User = Depends(current_active_user),
     db: AsyncSession = Depends(get_db),
 ):
-    print("I'm here")
     result = await db.execute(select(Project))
     return result.scalars().all()
 
 
-@router.post("/", response_model=Project, status_code=201)
+@router.post("/", response_model=ProjectRead, status_code=201)
 async def create_project(
-    project: Project,
+    project: ProjectCreate,
     user: User = Depends(current_active_user),
     db: AsyncSession = Depends(get_db),
 ):
     """
     Create new poject
     """
-    project = Project(name="project", project_dir="/tpm/")
-    db.add(project)
+    db_project = Project.from_orm(project)
+
+    db_project.user_owner_id = user.id
+    db.add(db_project)
     await db.commit()
-    await db.refresh(project)
-    return project
+    await db.refresh(db_project)
+    return db_project
 
 
 @router.post("/apply/{project_slug}/{dataset_id}")
--- fractal/server/app/models/project.py
+++ fractal/server/app/models/project.py
@@ -1,13 +1,48 @@
 from typing import Optional
 
+from pydantic import root_validator
+from pydantic import UUID4
 from sqlmodel import Field
+from sqlmodel import Relationship
 from sqlmodel import SQLModel
 
+from .security import UserOAuth
 
-class Project(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+
+def slugify(value: str):
+    return value.lower().replace(" ", "_")
+
+
+class ProjectBase(SQLModel):
     name: str
     project_dir: str
 
-    # user_owner_id: UUID4 = Field(foreign_key="user_oauth.id")
-    # user: Optional[UserOAuth] = Relationship(back_populates="oauth_accounts")
+
+class ProjectCreate(ProjectBase):
+    slug: Optional[str] = Field(sa_column_kwargs={"unique": True})
+
+    @root_validator(pre=True)
+    def compute_slug_if_not_provided(cls, values):
+        """
+        Creates the project's slug
+
+        Or double checks the user provided one if any.
+        """
+        slug = values.get("slug", None)
+        if not slug:
+            slug = values["name"]
+            values["slug"] = slugify(slug)
+        return values
+
+
+class Project(ProjectBase, table=True):  # type: ignore
+    id: Optional[int] = Field(default=None, primary_key=True)
+    slug: str
+
+    user_owner_id: Optional[UUID4] = Field(foreign_key="user_oauth.id")
+    user: Optional[UserOAuth] = Relationship()
+
+
+class ProjectRead(ProjectBase):
+    id: int
+    slug: str
