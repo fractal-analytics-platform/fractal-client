c54112d0fb3e1d6aec26f8b22e505ef48d0e27eb
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-06-30 19:26:50 +0200

    access control on project get
    and models for dataset and resource

--- fractal/server/app/api/v1/project.py
+++ fractal/server/app/api/v1/project.py
@@ -2,10 +2,13 @@ from typing import List
 
 from fastapi import APIRouter
 from fastapi import Depends
-from sqlalchemy.future import select
+from fastapi import HTTPException
+from fastapi import status
+from sqlmodel import select
 
 from ...db import AsyncSession
 from ...db import get_db
+from ...models.project import Dataset
 from ...models.project import Project
 from ...models.project import ProjectCreate
 from ...models.project import ProjectRead
@@ -21,8 +24,29 @@ async def get_list_project(
     user: User = Depends(current_active_user),
     db: AsyncSession = Depends(get_db),
 ):
-    result = await db.execute(select(Project))
-    return result.scalars().all()
+    stm = select(Project).where(Project.user_owner_id == user.id)
+    res = await db.execute(stm)
+    project_list = res.scalars().all()
+    return project_list
+
+
+@router.get("/{project_id}", response_model=ProjectRead)
+async def get_project(
+    project_id: int,
+    user: User = Depends(current_active_user),
+    db: AsyncSession = Depends(get_db),
+):
+    project = await db.get(Project, project_id)
+    if not project:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Project not found"
+        )
+    if project.user_owner_id != user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not allowed on project",
+        )
+    return project
 
 
 @router.post("/", response_model=ProjectRead, status_code=201)
@@ -35,6 +59,7 @@ async def create_project(
     Create new poject
     """
     db_project = Project.from_orm(project)
+    db_project.dataset_list.append(Dataset(name=project.default_dataset_name))
 
     db_project.user_owner_id = user.id
     db.add(db_project)
@@ -43,24 +68,34 @@ async def create_project(
     return db_project
 
 
-@router.post("/apply/{project_slug}/{dataset_id}")
-async def apply_workflow():
-    raise NotImplementedError
-
-
-@router.post("/{project_slug}")
-async def add_dataset():
+@router.post("/{project_id}")
+async def add_dataset(
+    project_id: int,
+):
     """
     Add new dataset to current project
     """
     raise NotImplementedError
 
 
-@router.post("/{project_slug}/{dataset_id}/")
-async def add_resource():
+@router.post("/{project_id}/{dataset_id}")
+async def add_resource(
+    project_id: str,
+    dataset_id: int,
+    user: User = Depends(current_active_user),
+    db: AsyncSession = Depends(get_db),
+):
     raise NotImplementedError
 
 
-@router.patch("/{project_slug}/{dataset_id}")
-async def modify_dataset():
+@router.patch("/{project_id}/{dataset_id}")
+async def modify_dataset(
+    project_id: int,
+    dataset_id: int,
+):
+    raise NotImplementedError
+
+
+@router.post("/apply/{project_id}/{dataset_id}")
+async def apply_workflow():
     raise NotImplementedError
--- fractal/server/app/models/project.py
+++ fractal/server/app/models/project.py
@@ -1,3 +1,4 @@
+from typing import List
 from typing import Optional
 
 from pydantic import root_validator
@@ -13,13 +14,35 @@ def slugify(value: str):
     return value.lower().replace(" ", "_")
 
 
+class DatasetBase(SQLModel):
+    name: str
+    type: Optional[str]
+
+
 class ProjectBase(SQLModel):
     name: str
     project_dir: str
 
 
+class ResourceBase(SQLModel):
+    path: str
+
+
+class DatasetRead(DatasetBase):
+    id: int
+
+
+class Dataset(DatasetBase, table=True):  # type: ignore
+    id: Optional[int] = Field(default=None, primary_key=True)
+    project_id: int = Field(foreign_key="project.id")
+    resource_list: List["Resource"] = Relationship(
+        sa_relationship_kwargs={"lazy": "selectin"}
+    )
+
+
 class ProjectCreate(ProjectBase):
     slug: Optional[str] = Field(sa_column_kwargs={"unique": True})
+    default_dataset_name: Optional[str] = "default"
 
     @root_validator(pre=True)
     def compute_slug_if_not_provided(cls, values):
@@ -42,7 +65,21 @@ class Project(ProjectBase, table=True):  # type: ignore
     user_owner_id: Optional[UUID4] = Field(foreign_key="user_oauth.id")
     user: Optional[UserOAuth] = Relationship()
 
+    dataset_list: List[Dataset] = Relationship(
+        sa_relationship_kwargs={"lazy": "selectin"}
+    )
+
+
+class Resource(ResourceBase, table=True):  # type: ignore
+    id: Optional[int] = Field(default=None, primary_key=True)
+    dataset_id: int = Field(foreign_key="dataset.id")
+
 
 class ProjectRead(ProjectBase):
     id: int
     slug: str
+    dataset_list: List[DatasetRead] = []
+
+
+class ResourceRead(ResourceBase):
+    id: int
