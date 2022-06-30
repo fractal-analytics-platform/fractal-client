b63c183560be76e699d9833fbe8a508c2afe618c
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-06-30 12:34:06 +0000

    Set correct scales in image_labeling prototype (ref #64)

--- fractal/tasks/image_labeling.py
+++ fractal/tasks/image_labeling.py
@@ -11,11 +11,14 @@ This file is part of Fractal and was originally developed by eXact lab S.r.l.
 Institute for Biomedical Research and Pelkmans Lab from the University of
 Zurich.
 """
+import json
+
 import dask.array as da
 import zarr
 from cellpose import core
 from cellpose import models
 from ome_zarr.io import parse_url
+from ome_zarr.scale import Scaler
 from ome_zarr.writer import write_labels
 
 
@@ -27,20 +30,74 @@ def image_labeling(
     FIXME
     """
 
+    ref_level = 2  # FIXME: level for segmentation, now pinned
+    ind_channel = 0  # FIXME: channel for segmentation, now pinned
+
     # Load some level and some channel
-    # FIXME: this is now pinned to level 0 and channel 0
-    dapi_dset = da.from_zarr(zarrurl + "/0")[0]
+    data_zyx = da.from_zarr(f"{zarrurl}/{ref_level}")[ind_channel]
+    shape_zyx_segmented_level = data_zyx.shape
 
     # Some debugging info
-    print("LOADED ZARR")
-    print(dapi_dset.shape)
+    print("LOADED ZARR", shape_zyx_segmented_level)
     print()
+
+    # Extract num_levels
+    zattrs_file = f"{zarrurl}.zattrs"
+    with open(zattrs_file, "r") as jsonfile:
+        zattrs = json.load(jsonfile)
+        num_levels = len(zattrs["multiscales"][0]["datasets"])
+    print("num_levels", num_levels)
+    print()
+
+    cxy = 2  # Still needed, for the scaler
+    # FIXME: check that shapes change by x2 each time
+
+    # Define custom ome_zarr coordinate transformations
+    coordinatetransformations = []
+    for level in range(num_levels):
+
+        zarray_file = f"{zarrurl}{level}/.zarray"
+        with open(zarray_file, "r") as jsonfile:
+            shape_zxy = json.load(jsonfile)["shape"][1:]
+        if level == 0:
+            shape_zxy_highres = shape_zxy[:]
+            global_prefactor_y = (
+                shape_zxy_highres[1] / shape_zyx_segmented_level[1]
+            )
+            global_prefactor_x = (
+                shape_zxy_highres[2] / shape_zyx_segmented_level[2]
+            )
+
+        factor_y = global_prefactor_y * shape_zxy_highres[1] / shape_zxy[1]
+        factor_x = global_prefactor_x * shape_zxy_highres[2] / shape_zxy[2]
+        factors = [1.0, factor_y, factor_x]
+
+        # Alternative way
+        # factor = cxy ** (level + ref_level) * 1.0
+        # factors = [1.0, factor, factor]
+
+        print(f"level {level}, factors {factors}")
+        print()
+
+        coordinatetransformations.append(
+            [
+                {
+                    "type": "scale",
+                    "scale": factors,
+                }
+            ]
+        )
+
+    # Define custom ome_zarr scaler
+    our_scaler = Scaler()
+    our_scaler.downscale = cxy
+    our_scaler.max_layer = num_levels - 1
 
     # Perform segmentation
     use_gpu = core.use_gpu()
     model = models.Cellpose(gpu=use_gpu, model_type="nuclei")
     mask, flows, styles, diams = model.eval(
-        dapi_dset, channels=[0, 0], do_3D=True, net_avg=False, augment=False
+        data_zyx, channels=[0, 0], do_3D=True, net_avg=False, augment=False
     )
 
     # Some debugging info
@@ -55,12 +112,16 @@ def image_labeling(
     store = parse_url(zarrurl, mode="w").store
     root = zarr.group(store=store)
     label_name = "label_image"
-    label_axes = [
-        "z",
-        "y",
-        "x",
-    ]  # could change if e.g. a 2D image was processed => can we infer this?
-    write_labels(mask, group=root, name=label_name, axes=label_axes)
+    label_axes = ["z", "y", "x"]  # FIXME: should be inferred (2D vs 3D)
+
+    write_labels(
+        mask,
+        group=root,
+        name=label_name,
+        axes=label_axes,
+        coordinate_transformations=coordinatetransformations,
+        scaler=our_scaler,
+    )
 
 
 if __name__ == "__main__":
