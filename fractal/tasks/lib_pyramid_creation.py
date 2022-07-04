9c376da690deea99a807b3cef6e506994093b291
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-07-04 07:57:57 +0000

    Introduce pyramid-creation function for 3D data (ref #53)

--- fractal/tasks/lib_pyramid_creation.py
+++ fractal/tasks/lib_pyramid_creation.py
@@ -104,3 +104,83 @@ def create_pyramid(
         pyramid.append(data_czyx_final)
 
     return pyramid
+
+
+def create_pyramid_3D(
+    data_zyx,
+    coarsening_z=1,
+    coarsening_xy=1,
+    num_levels=1,
+    chunk_size_x=None,
+    chunk_size_y=None,
+    num_channels=None,
+):
+
+    """
+    Take a three-dimensional array and build a pyramid of coarsened levels
+
+    :param data_czyx: input data
+    :type data_czyx: dask array
+    :param coarsening_z: coarsening factor along Z
+    :type coarsening_z: int
+    :param coarsening_xy: coarsening factor along X and Y
+    :type coarsening_xy: int
+    :param num_levels: number of levels in the zarr pyramid
+    :type num_levels: int
+    :param chunk_size_x: chunk size along X
+    :type chunk_size_x: int
+    :param chunk_size_y: chunk size along Y
+    :type chunk_size_y: int
+    :param num_channels: number of channels
+    :type num_channels: int
+    """
+
+    # Check that input has the right shape
+    if len(data_zyx.shape) != 3:
+        raise Exception("Error in create_pyramid: data_zyx has wrong shape")
+
+    # Check rechunking options
+    apply_rechunking = True
+    if chunk_size_x is None or chunk_size_y is None:
+        apply_rechunking = False
+
+    # Coarsen globally along Z direction
+    if coarsening_z > 1:
+        if data_zyx.shape[0] < coarsening_z:
+            raise Exception(
+                f"ERROR: coarsening_z={coarsening_z} "
+                f"but data_zyx.shape={data_zyx.shape}"
+            )
+        data_zyx = da.coarsen(
+            np.min, data_zyx, {1: coarsening_z}, trim_excess=True
+        )
+
+    # Create pyramid of XY-coarser levels
+    pyramid = [data_zyx]
+    for level in range(1, num_levels):
+        # Check that we can produce the next level
+        if min(pyramid[level - 1].shape[1:]) < coarsening_xy:
+            raise Exception(
+                f"ERROR: at level={level}, "
+                f"coarsening_xy={coarsening_xy} "
+                "but level-1 shape is "
+                f"{pyramid[level - 1].shape}"
+            )
+        # Coarsen (level-1)-th level into level-th level
+        data_zyx_new = da.coarsen(
+            np.mean,
+            pyramid[level - 1],
+            {1: coarsening_xy, 2: coarsening_xy},
+            trim_excess=True,
+        )
+        # Rechunk if needed
+        if apply_rechunking:
+            data_zyx_final = data_zyx_new.rechunk(
+                {1: chunk_size_y, 2: chunk_size_x}, balance=True
+            )
+        else:
+            data_zyx_final = data_zyx_new
+        # Append new level
+        pyramid.append(data_zyx_final)
+
+    return pyramid
