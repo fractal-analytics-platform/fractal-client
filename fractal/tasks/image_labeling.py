e76dddee67944bd2f43107277ec3129cb7dbab94
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-07-14 14:38:23 +0000

    Image labeling new version (without relabeling)

--- fractal/tasks/image_labeling.py
+++ fractal/tasks/image_labeling.py
@@ -11,20 +11,17 @@ This file is part of Fractal and was originally developed by eXact lab S.r.l.
 Institute for Biomedical Research and Pelkmans Lab from the University of
 Zurich.
 """
-import itertools
 import json
-import shutil
 import time
 from concurrent.futures import ThreadPoolExecutor
 
 import dask
 import dask.array as da
 import numpy as np
-import zarr
 from cellpose import core
 from cellpose import models
 
-from fractal.tasks.lib_pyramid_creation import create_pyramid_3D
+from fractal.tasks.lib_pyramid_creation import write_pyramid
 
 
 def segment_FOV(
@@ -59,6 +56,7 @@ def segment_FOV(
         diameter=diameter,
         anisotropy=anisotropy,
         cellprob_threshold=cellprob_threshold,
+        # batch_size=16,
     )
     if not do_3D:
         mask = np.expand_dims(mask, axis=0)
@@ -73,6 +71,10 @@ def segment_FOV(
             f" mask dtype: {mask.dtype} (before recast to {label_dtype}),"
             f" max(mask): {np.max(mask)}\n"
         )
+        # out.write(
+        #    f"[{chunk_location}] "
+        #    f" flows shape: {np.array(flows[1]).shape},"
+        #    f" flows dtype: {np.array(flows[1]).dtype}\n")
 
     return mask.astype(label_dtype)
 
@@ -83,8 +85,8 @@ def image_labeling(
     labeling_level=0,
     labeling_channel=None,
     chl_list=None,
-    num_threads=2,
-    relabeling=True,
+    num_threads=1,
+    relabeling=False,
     # More parameters
     anisotropy=None,
     diameter=None,
@@ -156,12 +158,6 @@ def image_labeling(
     if len(set(chunks_x)) != 1 or chunks_x[0] != img_size_x:
         raise Exception(f"Error in image_labeling, chunks_x: {chunks_x}")
 
-    # Rechunk to go from single 2D FOVs to 3D columns
-    # Note: this is irrelevant, in the 2D case
-    data_zyx_rechunked = da.rechunk(
-        data_zyx, chunks=(nz, img_size_y, img_size_x)
-    )
-
     # Initialize cellpose
     use_gpu = core.use_gpu()
     model = models.Cellpose(gpu=use_gpu, model_type="nuclei")
@@ -170,159 +166,41 @@ def image_labeling(
     with open("LOG_image_labeling", "w") as out:
         out.write(f"Start image_labeling task for {zarrurl}\n")
         out.write(f"relabeling: {relabeling}\n")
-        out.write(f"use_gpu: {use_gpu}\n")
         out.write("Total well shape/chunks:\n")
-        out.write(f"{data_zyx_rechunked.shape}\n")
-        out.write(f"{data_zyx_rechunked.chunks}\n\n")
+        out.write(f"{data_zyx.shape}\n")
+        out.write(f"{data_zyx.chunks}\n\n")
 
     # Map labeling function onto all chunks (i.e., FOV colums)
-    mask_rechunked = data_zyx_rechunked.map_blocks(
-        segment_FOV,
-        chunks=data_zyx_rechunked.chunks,
-        meta=np.array((), dtype=label_dtype),
-        model=model,
-        do_3D=do_3D,
-        anisotropy=anisotropy,
-        label_dtype=label_dtype,
+    mask = (
+        data_zyx.rechunk((nz, img_size_y, img_size_x))
+        .map_blocks(
+            segment_FOV,
+            meta=np.array((), dtype=label_dtype),
+            model=model,
+            do_3D=do_3D,
+            anisotropy=anisotropy,
+            label_dtype=label_dtype,
+        )
+        .rechunk((1, img_size_y, img_size_x))
     )
 
-    # Rechunk to get back to the original chunking (with separate Z planes)
-    mask = mask_rechunked.rechunk(data_zyx.chunks)
+    with open("LOG_image_labeling", "a") as out:
+        out.write(
+            f"After map_block, mask will have shape {mask.shape} "
+            f"and chunks {mask.chunks}\n\n"
+        )
 
-    # Write zattrs for labels and for specific label
-    # FIXME deal with: (1) many channels, (2) overwriting
-    labels_group = zarr.group(f"{zarrurl}labels")
-    labels_group.attrs["labels"] = [label_name]
-    label_group = labels_group.create_group(label_name)
-    label_group.attrs["image-label"] = {"version": "0.4"}
-    label_group.attrs["multiscales"] = [
-        {
-            "name": label_name,
-            "version": "0.4",
-            "axes": [
-                {"name": axis_name, "type": "space"}
-                for axis_name in ["z", "y", "x"]
-            ],
-            "datasets": [
-                {"path": f"{ind_level}"} for ind_level in range(num_levels)
-            ],
-        }
-    ]
-
+    # Construct resolution pyramid
     with dask.config.set(pool=ThreadPoolExecutor(num_threads)):
-        level0 = mask.to_zarr(
-            zarrurl,
-            component=f"labels/{label_name}/{0}",
-            dimension_separator="/",
-            return_stored=True,
-            # compute=True, #FIXME ???
-        )
-
-    # At this point, cellpose executed and data for level=0 are on disk
-
-    if not relabeling:
-        # Construct resolution pyramid
-        pyramid = create_pyramid_3D(
-            level0,
+        write_pyramid(
+            mask,
+            newzarrurl=f"{zarrurl}labels/{label_name}/",
+            overwrite=False,
             coarsening_xy=coarsening_xy,
             num_levels=num_levels,
-            chunk_size_x=img_size_x,
-            chunk_size_y=img_size_y,
-            aggregation_function=np.max,
+            chunk_size_x=2560,
+            chunk_size_y=2160,
         )
-
-        # Write data into output zarr
-        for ind_level in range(1, num_levels):
-            pyramid[ind_level].astype(label_dtype).to_zarr(
-                zarrurl,
-                component=f"labels/{label_name}/{ind_level}",
-                dimension_separator="/",
-            )
-
-    else:
-
-        with open("LOG_image_labeling", "a") as out:
-            out.write("Start relabeling\n")
-
-        mask = da.from_zarr(zarrurl, component=f"labels/{label_name}/{0}")
-        mask_rechunked = mask.rechunk(data_zyx_rechunked.chunks)
-        newmask_rechunked = da.empty(
-            shape=mask_rechunked.shape,
-            chunks=mask_rechunked.chunks,
-            dtype=label_dtype,
-        )
-
-        # Sequential relabeling
-        # https://stackoverflow.com/a/72018364/19085332
-        num_labels_tot = 0
-        num_labels_column = 0
-        for inds in itertools.product(
-            *map(range, mask_rechunked.blocks.shape)
-        ):
-
-            # Select a specific chunk (=column in 3D, =image in 2D)
-            column_mask = mask_rechunked.blocks[inds].compute()
-            num_labels_column = np.max(column_mask)
-
-            # Apply re-labeling and update total number of labels
-            shift = np.zeros_like(column_mask, dtype=label_dtype)
-            shift[column_mask > 0] = num_labels_tot
-            num_labels_tot += num_labels_column
-
-            with open("LOG_image_labeling", "a") as out:
-                out.write(
-                    f"Chunk {inds}, "
-                    f"num_labels_column={num_labels_column}, "
-                    f"num_labels_tot={num_labels_tot}\n"
-                )
-
-            # Check that total number of labels is under control
-            if num_labels_tot > np.iinfo(label_dtype).max - 1000:
-                raise Exception(
-                    "ERROR in re-labeling:\n"
-                    f"Reached {num_labels_tot} labels, "
-                    f"but dtype={label_dtype}"
-                )
-            # Re-assign the chunk to the new array
-            start_z = inds[0] * nz
-            end_z = (inds[0] + 1) * nz
-            start_y = inds[1] * img_size_y
-            end_y = (inds[1] + 1) * img_size_y
-            start_x = inds[2] * img_size_x
-            end_x = (inds[2] + 1) * img_size_x
-            newmask_rechunked[start_z:end_z, start_y:end_y, start_x:end_x] = (
-                column_mask + shift
-            )
-
-        newmask = newmask_rechunked.rechunk(data_zyx.chunks)
-
-        # FIXME: this is ugly
-        shutil.rmtree(zarrurl + f"labels/{label_name}/{0}")
-
-        level0 = newmask.to_zarr(
-            zarrurl,
-            component=f"labels/{label_name}/{0}",
-            dimension_separator="/",
-            return_stored=True,
-        )
-
-        # Construct resolution pyramid
-        pyramid = create_pyramid_3D(
-            level0,
-            coarsening_xy=coarsening_xy,
-            num_levels=num_levels,
-            chunk_size_x=img_size_x,
-            chunk_size_y=img_size_y,
-            aggregation_function=np.max,
-        )
-
-        # Write data into output zarr
-        for ind_level in range(1, num_levels):
-            pyramid[ind_level].astype(label_dtype).to_zarr(
-                zarrurl,
-                component=f"labels/{label_name}/{ind_level}",
-                dimension_separator="/",
-            )
 
 
 if __name__ == "__main__":
@@ -350,6 +228,12 @@ if __name__ == "__main__":
         "--labeling_channel",
         help="name of channel for labeling (e.g. A01_C01)",
     )
+    parser.add_argument(
+        "--num_threads",
+        default=1,
+        type=int,
+        help="TBD",
+    )
 
     args = parser.parse_args()
     image_labeling(
@@ -357,5 +241,6 @@ if __name__ == "__main__":
         coarsening_xy=args.coarsening_xy,
         chl_list=args.chl_list,
         labeling_channel=args.labeling_channel,
+        num_threads=args.num_threads,
         # FIXME: more arguments
     )
