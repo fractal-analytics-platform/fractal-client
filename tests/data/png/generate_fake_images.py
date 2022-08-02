ef8bf6f7552031aa30b435d4fa85a78e34641289
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-08-02 15:29:17 +0200

    Fix test images

--- tests/data/png/generate_fake_images.py
+++ tests/data/png/generate_fake_images.py
@@ -1,8 +1,8 @@
 import numpy as np
 from PIL import Image
 
-im1 = np.arange(2560 * 2160, dtype=np.uint16).reshape(2560, 2160)
-im2 = np.arange(2560 * 2160, dtype=np.uint16).reshape(2160, 2560).T
+im1 = np.arange(2560 * 2160, dtype=np.uint16).reshape(2160, 2560)
+im2 = np.arange(2560 * 2160, dtype=np.uint16).reshape(2560, 2160).T
 Image.fromarray(im1).save(
     "myplate_B03_T0001F001L01A01Z01C01.png", optimize=True
 )
