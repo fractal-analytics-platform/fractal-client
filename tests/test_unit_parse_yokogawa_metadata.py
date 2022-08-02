19c48b5a0eb50a51353f0604a849d2458b44cd27
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-08-02 12:08:23 +0200

    Update example with mlf/mrf files (ref #130)

--- tests/test_unit_parse_yokogawa_metadata.py
+++ tests/test_unit_parse_yokogawa_metadata.py
@@ -1,11 +1,8 @@
 import os
 
-import pytest
-
 from fractal.tasks.metadata_parsing import parse_yokogawa_metadata
 
 
-@pytest.mark.skip("Skip due to issue 130")
 def test_parse_yokogawa_metadata():
     # General variables and paths (relative to mwe_fractal folder)
     rootdir, relative_dir = os.getcwd().split("mwe_fractal")
@@ -19,7 +16,7 @@ def test_parse_yokogawa_metadata():
     metadata, num = parse_yokogawa_metadata(
         path + "MeasurementDetail.mrf", path + "MeasurementData.mlf"
     )
-    assert num == 2
+    assert num == 4
 
 
 if __name__ == "__main__":
