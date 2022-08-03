ee39742730f80fabe153f27474991872da972092
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-08-03 07:52:13 +0200

    Clean up of test folder
    * Rename some files, including test_unit_(...) [ref #130];
    * Skip task unit tests that will be deprecated in new architecture

--- tests/test_unit_parse_yokogawa_metadata.py
+++ tests/test_unit_parse_yokogawa_metadata.py
@@ -1,16 +1,142 @@
+"""
+Copyright 2022 (C) Friedrich Miescher Institute for Biomedical Research and
+University of Zurich
+
+Original authors:
+Joel Lüthi  <joel.luethi@fmi.ch>
+
+This file is part of Fractal and was originally developed by eXact lab S.r.l.
+<exact-lab.it> under contract with Liberali Lab from the Friedrich Miescher
+Institute for Biomedical Research and Pelkmans Lab from the University of
+Zurich.
+"""
 import os
+
+import numpy as np
+import pytest
 
 from fractal.tasks.metadata_parsing import parse_yokogawa_metadata
 
+# General variables and paths (relative to mwe_fractal folder)
+testdir = os.path.dirname(__file__)
 
-def test_parse_yokogawa_metadata():
-    testdir = os.path.dirname(__file__)
-    path = f"{testdir}/data/png/"
-    metadata, num = parse_yokogawa_metadata(
-        path + "MeasurementDetail.mrf", path + "MeasurementData.mlf"
-    )
-    assert num == 4
+path = f"{testdir}/data/metadata_files/"
+
+# Test set 1
+mlf_path_1 = f"{path}MeasurementData_SingleWell2Sites_MultiZ.mlf"
+mrf_path_1 = f"{path}MeasurementDetail.mrf"
+expected_files_1 = 4
+expected_shape_1 = (2, 10)
+x_mic_pos_1 = (-1448.3, -1032.3)
+y_mic_pos_1 = -1517.7
+pixel_size_z_1 = 1.0
+z_pixel_1 = 2
+pixel_size_x = 0.1625
+pixel_size_y = 0.1625
+x_pixel = 2560
+y_pixel = 2160
+bit_depth = 16
 
 
-if __name__ == "__main__":
-    test_parse_yokogawa_metadata()
+# Test set 2
+mlf_path_2 = f"{path}MeasurementData_2x2_well.mlf"
+mrf_path_2 = f"{path}MeasurementDetail_2x2_well.mrf"
+expected_files_2 = 120
+expected_shape_2 = (4, 10)
+x_mic_pos_2 = (-1448.3, -1032.3)
+y_mic_pos_2 = (-1517.7, -1166.7)
+pixel_size_z_2 = 1.0
+z_pixel_2 = 10
+
+# Test set 3
+mlf_path_3 = f"{path}MeasurementData_SingleWell2Sites_SingleZ.mlf"
+mrf_path_3 = f"{path}MeasurementDetail.mrf"
+expected_files_3 = 2
+expected_shape_3 = (2, 10)
+x_mic_pos_3 = (-1448.3, -1032.3)
+y_mic_pos_3 = -1517.7
+pixel_size_z_3 = 1.0
+z_pixel_3 = 1
+
+parameters = [
+    (
+        mlf_path_1,
+        mrf_path_1,
+        expected_files_1,
+        expected_shape_1,
+        x_mic_pos_1,
+        y_mic_pos_1,
+        pixel_size_z_1,
+        z_pixel_1,
+        pixel_size_x,
+        pixel_size_y,
+        x_pixel,
+        y_pixel,
+        bit_depth,
+    ),
+    (
+        mlf_path_2,
+        mrf_path_2,
+        expected_files_2,
+        expected_shape_2,
+        x_mic_pos_2,
+        y_mic_pos_2,
+        pixel_size_z_2,
+        z_pixel_2,
+        pixel_size_x,
+        pixel_size_y,
+        x_pixel,
+        y_pixel,
+        bit_depth,
+    ),
+    (
+        mlf_path_3,
+        mrf_path_3,
+        expected_files_3,
+        expected_shape_3,
+        x_mic_pos_3,
+        y_mic_pos_3,
+        pixel_size_z_3,
+        z_pixel_3,
+        pixel_size_x,
+        pixel_size_y,
+        x_pixel,
+        y_pixel,
+        bit_depth,
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "mlf_path, mrf_path, expected_files, expected_shape, x_mic_pos, "
+    "y_mic_pos, pixel_size_z, z_pixel, pixel_size_x, pixel_size_y, "
+    "x_pixel, y_pixel, bit_depth",
+    parameters,
+)
+def test_parse_yokogawa_metadata(
+    mlf_path,
+    mrf_path,
+    expected_files,
+    expected_shape,
+    x_mic_pos,
+    y_mic_pos,
+    pixel_size_z,
+    z_pixel,
+    pixel_size_x,
+    pixel_size_y,
+    x_pixel,
+    y_pixel,
+    bit_depth,
+):
+    site_metadata, total_files = parse_yokogawa_metadata(mrf_path, mlf_path)
+    assert total_files == expected_files
+    assert site_metadata.shape == expected_shape
+    assert np.allclose(site_metadata["x_micrometer"].unique(), x_mic_pos)
+    assert np.allclose(site_metadata["y_micrometer"].unique(), y_mic_pos)
+    assert np.allclose(site_metadata["pixel_size_z"], pixel_size_z)
+    assert np.allclose(site_metadata["z_pixel"], z_pixel)
+    assert np.allclose(site_metadata["pixel_size_x"], pixel_size_x)
+    assert np.allclose(site_metadata["pixel_size_y"], pixel_size_y)
+    assert np.allclose(site_metadata["x_pixel"], x_pixel)
+    assert np.allclose(site_metadata["y_pixel"], y_pixel)
+    assert np.allclose(site_metadata["bit_depth"], bit_depth)
