88c76518893dd17ed5e5e837828c4705b3298561
mfranzon <franzonmarco94@gmail.com>
2022-08-03 09:31:38 +0200

    add modify dataset function, show resources

--- fractal/client/client.py
+++ fractal/client/client.py
@@ -24,6 +24,7 @@ from rich.table import Table
 
 from ._auth import AuthToken
 from .config import settings
+from fractal.common.models import ResourceRead
 
 console = Console()
 
@@ -103,7 +104,7 @@ async def project_list():
         project_list = [ProjectRead(**item) for item in data]
 
         table = Table(title="Project List")
-        table.add_column("ID", style="cyan", no_wrap=True)
+        table.add_column("Id", style="cyan", no_wrap=True)
         table.add_column("Name", style="magenta")
         table.add_column("Proj. Dir.", justify="right", style="green")
         table.add_column("Dataset list", style="white")
@@ -201,5 +202,107 @@ async def dataset_show(project_id: int, dataset_name: str) -> None:
             for project in projects
             if project["id"] == project_id
         ][0]
-        dataset = [ds for ds in dataset_list if ds["name"] == dataset_name]
-        print_json(data=dataset)
+        dataset = [ds for ds in dataset_list if ds["name"] == dataset_name][0]
+
+        table = Table(title="Dataset")
+        table.add_column("Id", style="cyan", no_wrap=True)
+        table.add_column("Name", justify="right", style="green")
+        table.add_column("Type", style="white")
+        table.add_column("Meta", justify="center")
+
+        table.add_row(
+            str(dataset["id"]),
+            dataset["name"],
+            dataset["type"],
+            str(dataset["meta"]),
+        )
+
+        console.print(table)
+
+
+@dataset.command(name="add-resource")
+@click.argument("project_id", required=True, type=int, nargs=1)
+@click.argument("dataset_id", required=True, type=int, nargs=1)
+@click.argument("path", required=True, type=str, nargs=1)
+@click.option("--glob_pattern", required=True, type=str, default="", nargs=1)
+async def add_resource(
+    project_id: int, dataset_id: int, path: str, glob_pattern: str
+):
+
+    """
+    Add a new resource to an exisisting dataset
+    """
+
+    from fractal.common.models import ResourceCreate
+
+    resource = ResourceCreate(path=path, glob_pattern=glob_pattern)
+
+    async with httpx.AsyncClient() as client:
+        auth = AuthToken(client=client)
+        res = await client.post(
+            f"{settings.BASE_URL}/project/{project_id}/{dataset_id}",
+            json=resource.dict(),
+            headers=await auth.header(),
+        )
+
+        print_json(data=res.json())
+
+
+@dataset.command(name="show-resources")
+@click.argument("project_id", required=True, type=int, nargs=1)
+@click.argument("dataset_id", required=True, type=int, nargs=1)
+async def get_resource(
+    project_id: int,
+    dataset_id: int,
+):
+
+    async with httpx.AsyncClient() as client:
+        auth = AuthToken(client=client)
+        res = await client.get(
+            f"{settings.BASE_URL}/project/{project_id}/{dataset_id}",
+            headers=await auth.header(),
+        )
+        data = res.json()
+        resource_list = [ResourceRead(**item) for item in data]
+
+        table = Table(title="Resource List")
+        table.add_column("Id", style="cyan", no_wrap=True)
+        table.add_column("Path", justify="right", style="green")
+        table.add_column("Dataset Id", style="white")
+
+        for r in resource_list:
+
+            table.add_row(str(r.id), r.path, str(r.dataset_id))
+
+        console.print(table)
+
+
+@dataset.command(name="modify-dataset")
+@click.argument("project_id", required=True, type=int, nargs=1)
+@click.argument("dataset_id", required=True, type=int, nargs=1)
+@click.option(
+    "--name_dataset",
+    required=True,
+    nargs=1,
+)
+@click.option(
+    "--meta",
+    required=True,
+    type=click.File("rb"),
+    nargs=1,
+)
+@click.option(
+    "--type",
+    required=True,
+    nargs=1,
+    help=("The type of objects into the dataset"),
+)
+async def modity_update(
+    project_id: int,
+    dataset_id: int,
+    name_dataset: str = "",
+    meta: Dict = None,
+    type: str = "",
+):
+
+    pass
