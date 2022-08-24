ea1bac3415441fdcc96241f11c3d0d40206e1cc5
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-08-24 13:37:22 +0000

    .gitignore

--- examples/new_architecture_one_user_two_workflows/server_setup.sh
+++ examples/new_architecture_one_user_two_workflows/server_setup.sh
@@ -18,5 +18,8 @@ poetry run alembic upgrade head
 
 cd $HERE
 
+echo -e "DEPLOYMENT_TYPE=testing\nJWT_SECRET_KEY=secret\nDATA_DIR_ROOT=/tmp/\nSQLITE_PATH=./data/fractal_server.db" > .fractal_server.env
+echo -e "FRACTAL_USER=test@me.com\nFRACTAL_PASSWORD=test" > .fractal.env
+
 rm -r data
 mv $ROOTDIR/data .
