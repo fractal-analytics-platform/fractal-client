ba8f8cf2a50136891a95d8d5f2e174da340b5be1
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-08-26 10:15:47 +0200

    Update examples after 61cf75d21c69b6701aa5ecf7c7975d507c253f89

--- examples/new_architecture_two_users_two_workflows/user1/apply_workflow_from_client_1.sh
+++ examples/new_architecture_two_users_two_workflows/user1/apply_workflow_from_client_1.sh
@@ -7,47 +7,45 @@ TMPDIR=user$LABEL/tmp-proj-$LABEL
 rm -r ../$TMPDIR
 mkdir ../$TMPDIR
 
+PROJECT_NAME="myproj"
+DATASET_IN_NAME="in-ds-$LABEL"
+DATASET_OUT_NAME="out-ds-$LABEL"
+WORKFLOW_NAME="My WF $LABEL"
+
 # Create project
-poetry run client project new test-prj $TMPDIR
+poetry run client project new $PROJECT_NAME $TMPDIR
 
 TESTDATA=../../tests/data
-WFNAME="My WF $LABEL"
-
-
-PROJECT_ID=1
-DATASET_ID=1
-DATASET_ID_OUT=2
-WORKFLOW_ID=4
 
 # Update dataset info
-poetry run client dataset modify-dataset $PROJECT_ID $DATASET_ID --name_dataset test-ds-$LABEL --type image --read_only true
+poetry run client dataset modify-dataset $PROJECT_NAME "default" --new_dataset_name $DATASET_IN_NAME --type image --read_only true
 
 # Add resource to dataset
-poetry run client dataset add-resource $PROJECT_ID $DATASET_ID ${TESTDATA}/png/ --glob_pattern *.png
+poetry run client dataset add-resource $PROJECT_NAME $DATASET_IN_NAME ${TESTDATA}/png/ --glob_pattern *.png
 
 # Add output dataset
-poetry run client project add-dataset $PROJECT_ID out-ds-$LABEL --type zarr
-poetry run client dataset add-resource $PROJECT_ID $DATASET_ID_OUT ${TMPDIR}/out-ds-$LABEL --glob_pattern *.zarr
+poetry run client project add-dataset $PROJECT_NAME $DATASET_OUT_NAME --type zarr
+poetry run client dataset add-resource $PROJECT_NAME $DATASET_OUT_NAME ${TMPDIR}/$DATASET_OUT_NAME --glob_pattern *.zarr
 
 
 # Create workflow
-poetry run client task new "$WFNAME" workflow image zarr
+poetry run client task new "$WORKFLOW_NAME" workflow image zarr
 
 echo "{\"__PROVIDER_ARGS__\" : {\"max_blocks\": 10}}" > /tmp/args_wf_${LABEL}.json
-poetry run client task modify-task $WORKFLOW_ID --default_args /tmp/args_wf_${LABEL}.json
+poetry run client task modify-task "$WORKFLOW_NAME" --default_args /tmp/args_wf_${LABEL}.json
 
 # Add subtasks (with args, if needed)
-poetry run client task add-subtask "$WFNAME" "Create OME-ZARR structure"
+poetry run client task add-subtask "$WORKFLOW_NAME" "Create OME-ZARR structure"
 echo "{\"parallelization_level\" : \"well\", \"rows\":2, \"cols\": 1}" > /tmp/args_yoko_${LABEL}.json
-poetry run client task add-subtask "$WFNAME" "Yokogawa to Zarr" --args_json /tmp/args_yoko_${LABEL}.json
+poetry run client task add-subtask "$WORKFLOW_NAME" "Yokogawa to Zarr" --args_json /tmp/args_yoko_${LABEL}.json
 
 # Apply workflow
-poetry run client workflow apply $PROJECT_ID $DATASET_ID $WORKFLOW_ID --output_dataset_id $DATASET_ID_OUT
+poetry run client workflow apply $PROJECT_NAME $DATASET_IN_NAME "$WORKFLOW_NAME" --output_dataset_name $DATASET_OUT_NAME
 
 # Show info
 poetry run client project list
-poetry run client dataset show $PROJECT_ID test-ds-$LABEL
-poetry run client dataset show-resources $PROJECT_ID $DATASET_ID
-poetry run client dataset show $PROJECT_ID out-ds-$LABEL
-poetry run client dataset show-resources $PROJECT_ID $DATASET_ID_OUT
+poetry run client dataset show $PROJECT_NAME $DATASET_IN_NAME
+poetry run client dataset show-resources $PROJECT_NAME $DATASET_IN_NAME
+poetry run client dataset show $PROJECT_NAME $DATASET_OUT_NAME
+poetry run client dataset show-resources $PROJECT_NAME $DATASET_OUT_NAME
 poetry run client task list
--- examples/new_architecture_two_users_two_workflows/user2/register_user.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-http POST localhost:8000/auth/register email=test2@me.com password=test2
