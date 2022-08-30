1851996b00a830cbb3ef2f5944a634f30f48fb80
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-08-30 12:26:32 +0200

    Update test/data/png script and images

--- tests/data/png/generate_fake_images.py
+++ tests/data/png/generate_fake_images.py
@@ -1,17 +1,17 @@
 import numpy as np
 from PIL import Image
 
-im1 = np.arange(2560 * 2160, dtype=np.uint16).reshape(2160, 2560)
-im2 = np.arange(2560 * 2160, dtype=np.uint16).reshape(2560, 2160).T
+im1 = np.arange(2560 * 2160, dtype=np.uint16).reshape(2160, 2560) // 100
+im2 = np.arange(2560 * 2160, dtype=np.uint16).reshape(2560, 2160).T // 100
 Image.fromarray(im1).save(
     "myplate_B03_T0002F001L01A01Z01C01.png", optimize=True
 )
-Image.fromarray(im1).save(
+Image.fromarray(im2).save(
     "myplate_B03_T0002F001L01A01Z02C01.png", optimize=True
 )
 Image.fromarray(im2).save(
     "myplate_B03_T0002F002L01A01Z01C01.png", optimize=True
 )
-Image.fromarray(im2).save(
+Image.fromarray(im1).save(
     "myplate_B03_T0002F002L01A01Z02C01.png", optimize=True
 )
