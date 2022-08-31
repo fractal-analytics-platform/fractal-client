a7db25b92bac882d6cb66d8e9d8465a6c58d5720
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-08-31 17:38:27 +0200

    checkout measurement.py task and its Sandbox from tasks branch (ref #164)

---
+++ fractal/tasks/Sandbox_measurement/call_measurement_3D.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+from fractal.tasks.measurement import measurement
+
+
+input_paths = [
+    Path(
+        "/data/active/fractal/tests/Temporary_data_UZH_1_well_2x2_sites/*.zarr"
+    )
+]
+component = "20200812-CardiomyocyteDifferentiation14-Cycle1.zarr/B/03/0"
+labeling_channel = "A01_C01"
+level = 0
+metadata = dict(coarsening_xy=2, channel_list=["A01_C01"])
+workflow_file = "regionprops_from_existing_labels.yaml"
+table_name = "nuclei"
+whole_well = False
+
+
+measurement(
+    input_paths=input_paths,
+    output_path=input_paths[0],
+    metadata=metadata,
+    component=component,
+    labeling_channel=labeling_channel,
+    level=level,
+    workflow_file=workflow_file,
+    table_name=table_name,
+    whole_well=whole_well,
+)
---
+++ fractal/tasks/Sandbox_measurement/sub_2D.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
+#SBATCH --partition=main
+#SBATCH --time=10:00:00
+
+date
+echo
+
+poetry run python call_measurement_2D.py
+
+echo
+date
