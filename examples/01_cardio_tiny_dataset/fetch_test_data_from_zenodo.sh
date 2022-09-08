075a48f4c10021e4ee87718232e5f4ea884b20c2
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-09-08 11:34:12 +0200

    Simplify fetch_test_data_from_zenodo.sh

--- examples/01_cardio_tiny_dataset/fetch_test_data_from_zenodo.sh
+++ examples/01_cardio_tiny_dataset/fetch_test_data_from_zenodo.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-DOIs="10.5281/zenodo.7059515"
-
-for DOI in $DOIs; do
-    CLEAN_DOI=${DOI/\//_}
-    echo $CLEAN_DOI
-    zenodo_get $DOI -o images/${CLEAN_DOI}
-done
+DOI="10.5281/zenodo.7059515"
+CLEAN_DOI=${DOI/\//_}
+zenodo_get $DOI -o images/${CLEAN_DOI}
