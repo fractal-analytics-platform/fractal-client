a9f0e8689aa7c5ae26f67f2d59b965afea026aa6
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-09-08 11:33:21 +0200

    Fix view_info.sh

--- examples/01_cardio_tiny_dataset/client/view_info.sh
+++ examples/01_cardio_tiny_dataset/client/view_info.sh
@@ -1,11 +1,9 @@
 # NOTE: This works if env variables are already defined,
 #       and if the user is already registered
 
-CMD="poetry run fractal"
-
-$CMD project list
-$CMD task list
-$CMD dataset show $PROJECT_NAME $DATASET_IN_NAME
-$CMD dataset show-resources $PROJECT_NAME $DATASET_IN_NAME
-$CMD dataset show $PROJECT_NAME $DATASET_OUT_NAME
-$CMD dataset show-resources $PROJECT_NAME $DATASET_OUT_NAME
+fractal project list
+fractal task list
+fractal dataset show $PROJECT_NAME $DATASET_IN_NAME
+fractal dataset show-resources $PROJECT_NAME $DATASET_IN_NAME
+fractal dataset show $PROJECT_NAME $DATASET_OUT_NAME
+fractal dataset show-resources $PROJECT_NAME $DATASET_OUT_NAME
