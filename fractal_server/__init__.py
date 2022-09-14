25025bd9af4f27fc4eaab260ad6d5f553515f61e
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-09-14 13:31:39 +0200

    remove development router

--- fractal_server/__init__.py
+++ fractal_server/__init__.py
@@ -18,13 +18,10 @@ def collect_routers(app: FastAPI) -> None:
     from .app.api import router_default
     from .app.api import router_v1
     from .app.security import auth_router
-    from .app.ui import router as ui_router
 
     app.include_router(router_default, prefix="/api")
     app.include_router(router_v1, prefix="/api/v1")
     app.include_router(auth_router, prefix="/auth", tags=["auth"])
-
-    app.include_router(ui_router)
 
 
 async def __on_startup():
