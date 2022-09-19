1113b24268abeca6f2ea02c4af3371e141e28606
mfranzon <franzonmarco94@gmail.com>
2022-09-19 14:46:28 +0200

    add task apply test

--- tests/test_task.py
+++ tests/test_task.py
@@ -13,3 +13,19 @@ async def test_task_list(clear_db, testserver, register_user, invoke):
     res.show()
     assert res.retcode == 0
     assert len(res.data) == 2
+
+
+async def test_task_apply(clear_db, testserver, register_user, invoke):
+    from devtools import debug
+
+    PROJECT_NAME = "project_name"
+    PROJECT_PATH = "project_path"
+    res = await invoke(f"project new {PROJECT_NAME} {PROJECT_PATH}")
+    project_id = res.data["id"]
+    dataset_id = res.data["dataset_list"][0]["id"]
+    res = await invoke(f"dataset edit {project_id} {dataset_id} --type image")
+    res = await invoke("task new mytask task image zarr mypackage.subpkg:foo")
+    res = await invoke("task list")
+    res.show()
+    res = await invoke("task apply project_name default default mytask ")
+    debug(res)
