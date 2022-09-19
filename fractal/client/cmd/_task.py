82dc5d226878b383e2a5adb329527573b805fccf
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-09-19 12:51:57 +0200

    add task commands

---
+++ fractal/client/cmd/_task.py
@@ -0,0 +1,73 @@
+import json
+from typing import Optional
+
+from ..authclient import AuthClient
+from ..config import settings
+from ..interface import RichJsonInterface
+from ..response import check_response
+from fractal.common.models import TaskCreate
+from fractal.common.models import TaskRead
+
+
+async def task_list(
+    client: AuthClient,
+    **kwargs,
+) -> RichJsonInterface:
+    res = await client.get(
+        f"{settings.BASE_URL}/task/",
+    )
+    task_list = check_response(res, expected_status_code=200)
+    return RichJsonInterface(retcode=0, data=task_list)
+
+
+async def task_new(
+    client: AuthClient,
+    *,
+    name: str,
+    resource_type: str,
+    input_type: str,
+    output_type: str,
+    module: str,
+    default_args: Optional[str] = None,
+    subtask_list: Optional[str] = None,
+    **kwargs,
+) -> RichJsonInterface:
+
+    if not default_args:
+        default_args_dict = {}
+    else:
+        default_args_dict = json.loads(default_args)
+    if not subtask_list:
+        subtask_list_list = []
+    else:
+        subtask_list_list = json.loads(subtask_list)
+
+    resource_type = resource_type.replace("_", " ")
+    task = TaskCreate(
+        name=name,
+        resource_type=resource_type,
+        input_type=input_type,
+        output_type=output_type,
+        default_args=default_args_dict,
+        module=module,
+        subtask_list=subtask_list_list,
+    )
+
+    res = await client.post(
+        f"{settings.BASE_URL}/task/",
+        json=task.dict(),
+    )
+    new_task = check_response(res, expected_status_code=201, coerce=TaskRead)
+    return RichJsonInterface(retcode=0, data=new_task.dict())
+
+
+async def task_edit():
+    pass
+
+
+async def task_add_subtask():
+    pass
+
+
+async def task_apply():
+    pass
