cd436a0f0788de3a0fe3382b6d09d4120ddce6c2
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-10-03 11:43:53 +0200

    Update examples 02 and 03

--- examples/03_cardio_dataset_one_2x2_well_3_channels_10_Z/aux_extract_id_for_core_task.py
+++
@@ -1,9 +0,0 @@
-import json
-import sys
-
-
-filename = sys.argv[1]
-with open(filename, "r") as fin:
-    tasks = json.load(fin)
-task = next(task for task in tasks if task["name"] == sys.argv[2])
-print(task["id"])
