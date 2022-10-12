1bee168435ca63ce7cf5d9c2f210dc8fbc8b52c6
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-10-12 10:39:02 +0200

    update fractal.common requirement

--- fractal_server/app/models/__init__.py
+++ fractal_server/app/models/__init__.py
@@ -1,4 +1,8 @@
+from fractal.common.models import *  # noqa F401
+from fractal.common.models import ResourceUpdate  # noqa F401
+
 from .project import *  # noqa: F403, F401
 from .security import *  # noqa: F403, F401
 from .task import *  # noqa: F403, F401
-from fractal.common.models import *  # noqa F401
+
+# TODO FIXME: in the next release this will be included in the __all__ tag
