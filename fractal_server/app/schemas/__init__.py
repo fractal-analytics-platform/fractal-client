10543c0c516e2993433f576193bb8b72c7decb08
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-10-13 19:48:05 +0200

    remove worker init
    as we are transitioning towards per-task environment

--- fractal_server/app/schemas/__init__.py
+++ fractal_server/app/schemas/__init__.py
@@ -42,7 +42,6 @@ class ApplyWorkflowBase(SQLModel):
     output_dataset_id: Optional[int]
     workflow_id: Optional[int]
     overwrite_input: bool = False
-    worker_init: Optional[str] = None
 
 
 class ApplyWorkflowCreate(ApplyWorkflowBase):
