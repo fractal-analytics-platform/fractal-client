f658e1012904ee336f99a9deae9ffe74528d7ff9
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-10-14 10:15:28 +0200

    refactor common runner interface
    ref #134

--- fractal_server/app/runner/__init__.py
+++ fractal_server/app/runner/__init__.py
@@ -1,9 +1,12 @@
+from ._common import auto_output_dataset  # noqa: F401
+from ._common import validate_workflow_compatibility  # noqa: F401
+
+
 RUNNER_BACKEND = "PARSL"
+
 
 if RUNNER_BACKEND == "PARSL":
     from .parsl import submit_workflow
-    from .parsl import auto_output_dataset  # noqa: F401
-    from .parsl import validate_workflow_compatibility  # noqa: F401
 elif RUNNER_BACKEND == "process":
     from .process import submit_workflow
 else:
---
+++ fractal_server/app/runner/_common.py
@@ -0,0 +1,74 @@
+from typing import Optional
+
+from ..models.project import Dataset
+from ..models.project import Project
+from ..models.task import Task
+
+
+async def auto_output_dataset(
+    *,
+    project: Project,
+    input_dataset: Dataset,
+    workflow: Task,
+    overwrite_input: bool = False,
+):
+    """
+    Determine the output dataset if it was not provided explicitly
+
+    Only datasets containing exactly one path can be used as output.
+
+    Returns
+    -------
+    output_dataset (Dataset):
+        the output dataset
+    """
+    if overwrite_input and not input_dataset.read_only:
+        input_paths = input_dataset.paths
+        if len(input_paths) != 1:
+            raise ValueError
+        output_dataset = input_dataset
+    else:
+        raise NotImplementedError
+
+    return output_dataset
+
+
+def validate_workflow_compatibility(
+    *,
+    input_dataset: Dataset,
+    workflow: Task,
+    output_dataset: Optional[Dataset] = None,
+):
+    """
+    Check compatibility of workflow and input / ouptut dataset
+    """
+    if (
+        workflow.input_type != "Any"
+        and workflow.input_type != input_dataset.type
+    ):
+        raise TypeError(
+            f"Incompatible types `{workflow.input_type}` of workflow "
+            f"`{workflow.name}` and `{input_dataset.type}` of dataset "
+            f"`{input_dataset.name}`"
+        )
+
+    if not output_dataset:
+        if input_dataset.read_only:
+            raise ValueError("Input dataset is read-only")
+        else:
+            input_paths = input_dataset.paths
+            if len(input_paths) != 1:
+                # Only single input can be safely transformed in an output
+                raise ValueError(
+                    "Cannot determine output path: multiple input "
+                    "paths to overwrite"
+                )
+            else:
+                output_path = input_paths[0]
+    else:
+        output_path = output_dataset.paths
+        if len(output_path) != 1:
+            raise ValueError(
+                "Cannot determine output path: Multiple paths in dataset."
+            )
+    return output_path
