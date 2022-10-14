ba075b97b34aad2d903fa08e2208d8f22ef28602
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-10-14 12:16:09 +0200

    Fix test_import_parsl_backend.py

--- tests/test_import_parsl_backend.py
+++ tests/test_import_parsl_backend.py
@@ -1,10 +1,12 @@
-import importlib
 import os
+import sys
 
 
 def test_import_parsl_backend():
-    import fractal_server.app.runner.parsl
+    import fractal_server.config
 
     os.environ.pop("DEPLOYMENT_TYPE")
-    importlib.reload(fractal_server.config)
+    del sys.modules["fractal_server.config"]
+    del fractal_server.config
+
     import fractal_server.app.runner.parsl
