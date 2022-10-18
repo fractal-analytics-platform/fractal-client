490c2cbecc5bfd9c667c07f9be17b29d7f4229f9
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-10-18 20:40:18 +0200

    STUB recursive submission and tests

--- fractal_server/app/runner/process/__init__.py
+++ fractal_server/app/runner/process/__init__.py
@@ -1,5 +1,9 @@
+import json
 import logging
 import subprocess  # nosec
+from concurrent.futures import Executor
+from concurrent.futures import Future
+from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
 from shlex import split as shlex_split
 from typing import Any
@@ -7,6 +11,10 @@ from typing import Dict
 from typing import List
 
 from ...models import Workflow
+from ...models import WorkflowTask
+from ..common import TaskParameterEncoder
+from ..common import TaskParameters
+
 
 """
 Process Bakend
@@ -18,12 +26,81 @@ Incidentally, it represents the reference implementation for a backend.
 """
 
 
-def _call_command_wrapper(cmd: str):
+class TaskExecutionError(RuntimeError):
+    def __init__(self, completed_process: subprocess.CompletedProcess):
+        self.completed_process = completed_process
+        super().__init__(completed_process.stderr.decode("utf-8"))
+
+
+def _call_command_wrapper(cmd: str) -> subprocess.CompletedProcess:
     """
     Call command and return stdout, stderr, retcode
     """
 
-    return subprocess.run(shlex_split(cmd), capture_output=True)  # nosec
+    result = subprocess.run(shlex_split(cmd), capture_output=True)  # nosec
+    if result.returncode != 0:
+        raise TaskExecutionError(result)
+    return result
+
+
+def call_single_task(
+    task: WorkflowTask,
+    task_pars: TaskParameters,
+    workflow_dir: Path = None,
+) -> TaskParameters:
+    from devtools import debug
+
+    if not workflow_dir:
+        raise RuntimeError
+    # assemble full args
+    args_dict = task_pars.dict(exclude={"logger"})
+    debug(task.arguments)
+    args_dict.update(task.arguments)
+    debug(args_dict)
+
+    # write args file
+    args_file_path = workflow_dir / f"{task.order}.args.json"
+    with open(args_file_path, "w") as f:
+        json.dump(args_dict, f, cls=TaskParameterEncoder)
+
+    # assemble full command
+    debug(task.task)
+    cmd = f"{task.task.command} -j {args_file_path}"
+    debug(cmd)
+    return _call_command_wrapper(cmd)
+
+
+def recursive_task_submission(
+    executor: Executor,
+    task_list: List[WorkflowTask],
+    task_pars: TaskParameters,
+) -> Future:
+    """
+    0: return a future with everything that is needed to submit first task of
+       workflow
+
+    n -> n+1: use output from n to submit task n+1
+    """
+    from devtools import debug
+
+    debug(f"TASK SUBMISSION: {[t.order for t in task_list]}")
+    try:
+        *dependent_tasks, this_task = task_list
+    except ValueError:
+        # 0
+        debug("0")
+        pseudo_future = Future()
+        pseudo_future.set_result(task_pars)
+        return pseudo_future
+
+    # n -> n+1
+    return executor.submit(
+        call_single_task,
+        task=this_task,
+        task_pars=recursive_task_submission(
+            executor=executor, task_list=dependent_tasks
+        ).result(),
+    )
 
 
 async def process_workflow(
@@ -35,4 +112,21 @@ async def process_workflow(
     logger: logging.Logger,
     username: str = None,
 ) -> Dict[str, Any]:
+
+    from devtools import debug
+
+    debug(workflow)
+
+    with ProcessPoolExecutor() as executor:
+        recursive_task_submission(
+            executor=executor,
+            task_list=workflow.task_list,
+            task_pars=TaskParameters(
+                input_paths=input_paths,
+                output_path=output_path,
+                metadata=input_metadata,
+                logger=logger,
+            ),
+        )
+
     raise NotImplementedError
--- tests/test_process_runner.py
+++ tests/test_process_runner.py
@@ -14,13 +14,17 @@ import logging
 from concurrent.futures import ThreadPoolExecutor
 
 from devtools import debug
+from pydantic import BaseModel
 
 import fractal_server.tasks.dummy as dummy
 from fractal_server.app.models import Task
 from fractal_server.app.models import Workflow
 from fractal_server.app.runner import set_job_logger
+from fractal_server.app.runner.common import TaskParameters
 from fractal_server.app.runner.process import _call_command_wrapper
+from fractal_server.app.runner.process import call_single_task
 from fractal_server.app.runner.process import process_workflow
+from fractal_server.tasks import dummy as dummy_module
 
 
 async def test_runner(db, project_factory, MockCurrentUser, tmp_path):
@@ -64,7 +68,7 @@ async def test_runner(db, project_factory, MockCurrentUser, tmp_path):
     await process_workflow(
         workflow=wf,
         input_paths=[tmp_path / "*.txt"],
-        output_path=[tmp_path / "out.txt"],
+        output_path=tmp_path / "out.txt",
         input_metadata={},
         logger=job_logger,
     )
@@ -81,3 +85,71 @@ async def test_command_wrapper():
     result = future.result()
     debug(result.stdout, result.stderr, result.returncode)
     assert dummy.__file__ in result.stdout.decode("utf-8")
+
+
+def test_recursive_task_submission():
+    from pydantic import BaseModel
+
+    class TaskParameters(BaseModel):
+        input_path: str
+        output_path: str
+        metadata: str
+
+    def recursive_task_submission(task_list, task_parameters):
+        try:
+            *depenency_tasks, this_task = task_list
+        except ValueError:
+            return task_parameters
+        dependency = recursive_task_submission(
+            depenency_tasks, task_parameters
+        )
+
+        this_task_parameters = dependency
+        debug(this_task, this_task_parameters)
+        return TaskParameters(
+            input_path=task_parameters.output_path,
+            output_path=task_parameters.output_path,
+            metadata=f"metaupdate {this_task}",
+        )
+
+    recursive_task_submission(
+        ["task0", "task1", "task2"],
+        task_parameters=TaskParameters(
+            input_path="input0",
+            output_path="output",
+            metadata="metadata0",
+        ),
+    )
+    assert False
+
+
+def test_call_single_task(tmp_path):
+    class MockTask(BaseModel):
+        command: str
+
+    class MockWorkflowTask(BaseModel):
+        order: int = 0
+        task: MockTask
+        arguments = {}
+
+    task = MockWorkflowTask(
+        task=MockTask(command=f"python {dummy_module.__file__}"),
+        arguments=dict(message="test"),
+        order=0,
+    )
+    task_pars = TaskParameters(
+        input_paths=[tmp_path],
+        output_path=tmp_path,
+        metadata={},
+        logger=logging.getLogger(),
+    )
+
+    debug(task)
+
+    out = call_single_task(
+        task=task, task_pars=task_pars, workflow_dir=tmp_path
+    )
+    debug(out)
+    debug(out.stderr.decode("utf-8"))
+    debug(out.returncode)
+    assert False
