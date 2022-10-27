8cfcd6b4d1f50ebd068b220d025ffb6787b40700
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-10-27 10:15:56 +0200

    simplify config validation
    in response to @tcompa's comment

--- fractal_server/config.py
+++ fractal_server/config.py
@@ -24,13 +24,11 @@ from pydantic import BaseModel
 from pydantic import BaseSettings
 from pydantic import Field
 from pydantic import root_validator
-from pydantic import validator
 
 import fractal_server
 
 
 T = TypeVar("T")
-_DEVNULL = Path("/dev/null")
 
 
 load_dotenv(".fractal_server.env")
@@ -150,7 +148,7 @@ class Settings(BaseSettings):
     ###########################################################################
     # FRACTAL SPECIFIC
     ###########################################################################
-    FRACTAL_ROOT: Path = _DEVNULL
+    FRACTAL_ROOT: Optional[Path]  # Path = _DEVNULL
     RUNNER_BACKEND: str = "process"
     RUNNER_ROOT_DIR: Path = Path("artifacts")
 
@@ -191,11 +189,6 @@ class Settings(BaseSettings):
                 SQLITE_PATH: str
 
             FRACTAL_ROOT: Path
-
-        @validator("FRACTAL_ROOT", allow_reuse=True)
-        def validate_not_devnull(cls, value):
-            if value == _DEVNULL:
-                raise ValueError("Must define `FRACTAL_ROOT` in environment")
 
         StrictSettings(**self.dict())
 
