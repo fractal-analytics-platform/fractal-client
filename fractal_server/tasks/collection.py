11d702db6ed019439f067db0dd6d75d551f0d71a
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-11-02 14:37:11 +0100

    move check to validator
    cf. https://github.com/fractal-analytics-platform/fractal-server/pull/198#discussion_r1009314663

--- fractal_server/tasks/collection.py
+++ fractal_server/tasks/collection.py
@@ -1,15 +1,13 @@
-"""
-Copyright 2022 (C) Friedrich Miescher Institute for Biomedical Research and
-University of Zurich
-
-Original authors:
-Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
-
-This file is part of Fractal and was originally developed by eXact lab S.r.l.
-<exact-lab.it> under contract with Liberali Lab from the Friedrich Miescher
-Institute for Biomedical Research and Pelkmans Lab from the University of
-Zurich.
-"""
+# Copyright 2022 (C) Friedrich Miescher Institute for Biomedical Research and
+# University of Zurich
+#
+# Original authors:
+# Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
+#
+# This file is part of Fractal and was originally developed by eXact lab S.r.l.
+# <exact-lab.it> under contract with Liberali Lab from the Friedrich Miescher
+# Institute for Biomedical Research and Pelkmans Lab from the University of
+# Zurich.
 import json
 from pathlib import Path
 from typing import List
@@ -82,12 +80,6 @@ def create_package_dir_pip(
     **_,
 ) -> Path:
     settings = Inject(get_settings)
-    # assemble installation path
-    package_path = Path(task_pkg.package)
-    if package_path.exists():
-        # The package is a local package in the filesystem rather than a
-        # remote PyPI package. Extract package and version from basename
-        package, version = _package_from_path(package_path)
 
     package_dir = f"{task_pkg.package}{task_pkg.version or ''}"
     if settings.FRACTAL_ROOT:
