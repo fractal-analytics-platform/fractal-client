e8e3c0772926758472373b1c2391ccc15b9c9057
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-11-06 20:51:38 +0100

    test list task and min server version
    ref #303

--- tests/test_task.py
+++ tests/test_task.py
@@ -4,9 +4,19 @@ import pytest
 from devtools import debug
 
 
-async def test_task_collect(
-    clear_db, testserver, register_user, invoke, testdata_path
+async def test_task_collection_and_list(
+    clear_db, testserver, register_user, invoke, testdata_path, temp_data_dir
 ):
+    """
+    GIVEN a pip installable package containing fractal-compatible tasks
+    WHEN the collection subcommand is called
+    THEN
+        * the collection is initiated in the background
+        * the server returns immediately and the client displays the
+
+    WHEN the list command is called
+    THEN the tasks collected are shown
+    """
     PACKAGE_NAME = testdata_path / "fractal_tasks_dummy-0.1.0-py3-none-any.whl"
 
     res0 = await invoke(f"task collect {PACKAGE_NAME}")
@@ -14,22 +24,27 @@ async def test_task_collect(
     res0.show()
     venv_path = res0.data["venv_path"]
 
-    res1 = await invoke(f"task check-collection {venv_path}")
-    debug(res1)
-    res1.show()
-    assert res1.data["status"] == "pending"
+    # cf. fixtures_testserver.py::testserver
+    FRACTAL_ROOT = temp_data_dir
+    collection_file = FRACTAL_ROOT / venv_path / "collection.json"
 
-    await asyncio.sleep(3)
+    while True:
+        res1 = await invoke(f"task check-collection {venv_path}")
+        debug(res1)
+        res1.show()
+        expected_status = "OK" if collection_file.exists() else "pending"
+        assert res1.data["status"] == expected_status
+        await asyncio.sleep(1)
+        if expected_status == "OK":
+            break
 
     res2 = await invoke(f"task check-collection {venv_path}")
     debug(res2)
     res2.show()
     assert res2.data["status"] == "OK"
 
+    # LIST
 
-async def test_task_list(clear_db, testserver, register_user, invoke):
-    raise NotImplementedError
-    # TODO add tasks
     res = await invoke("task list")
     res.show()
     assert res.retcode == 0
@@ -43,17 +58,13 @@ async def test_task_apply(clear_db, testserver, register_user, invoke):
     # add input resource
     # check that fractal_tasks_core is present (should be optional)
     # try to apply the dummy or another taks
-    assert False
+    raise NotImplementedError
 
 
+@pytest.mark.xfail
 async def test_edit_task(
     clear_db, testserver, register_user, invoke, clear_task_cache
 ):
+    # TODO:
+    # Decide what it means to edit a task
     raise NotImplementedError
-    # TODO create task
-    task_name = "FOO"
-
-    res = await invoke(f"task edit {task_name} --name 'new task name'")
-    res.show()
-    assert res.retcode == 0
-    assert res.data["name"] == "new task name"
