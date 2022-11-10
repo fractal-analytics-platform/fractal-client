dc35f7b46616326f89a502ba6983bf782e3f8610
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-11-10 15:52:42 +0100

    prepare for pg env vars

--- tests/fixtures_server.py
+++ tests/fixtures_server.py
@@ -39,10 +39,17 @@ def get_patched_settings(temp_path: Path):
     settings = Settings()
     settings.JWT_SECRET_KEY = "secret_key"
     settings.DEPLOYMENT_TYPE = "development"
+
     settings.DB_ENGINE = "sqlite"
     settings.SQLITE_PATH = (
         f"{temp_path.as_posix()}/_test.db?mode=memory&cache=shared"
     )
+
+    # settings.DB_ENGINE = "postgres"
+    # settings.POSTGRES_USER = "postgres"
+    # settings.POSTGRES_PASSWORD = "postgres"
+    # settings.POSTGRES_DB = "fractal"
+
     settings.FRACTAL_ROOT = temp_path
     settings.RUNNER_ROOT_DIR = temp_path / "artifacts"
     settings.FRACTAL_LOGGING_LEVEL = logging.DEBUG
@@ -120,7 +127,8 @@ async def db_session_maker(
     from sqlmodel import SQLModel
 
     async with db_engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
+        metadata = SQLModel.metadata
+        await conn.run_sync(metadata.create_all)
         async_session_maker = sessionmaker(
             db_engine, class_=AsyncSession, expire_on_commit=False
         )
