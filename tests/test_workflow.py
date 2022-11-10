a6cbcf52a2de53d2254fd65f289f938b3da99eaa
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-11-10 10:57:53 +0100

    BROKEN add test_edit_workflow_task (ref #313)

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -99,3 +99,45 @@ async def test_add_task(
     assert res.retcode == 0
     debug(res.data)
     assert res.data["task_list"][0]["args"] == custom_args
+
+
+async def test_edit_workflow_task(
+    clear_db,
+    testserver,
+    invoke,
+    register_user,
+    task_factory,
+    workflow_factory,
+    tmp_path,
+):
+    """
+    GIVEN a workflow
+    WHEN the client is invoked to add a task, including custom args
+    THEN
+        the WorkflowTask is correctly registered in the db, including custom
+        gargs
+    """
+    wf = await workflow_factory()
+    t = await task_factory()
+
+    # Create task, without overriding arguments
+    cmd = f"workflow add-task {wf.id} {t.id}"
+    res = await invoke(cmd)
+    assert res.retcode == 0
+
+    # New arguments to be used
+    custom_args = dict(custom="args")
+    args_file = tmp_path / "args_file.json"
+    with args_file.open("w") as f:
+        json.dump(custom_args, f)
+
+    # Edit workflow task
+    workflow_task_id = res.data["task_list"][0]["args"]
+    cmd = (
+        f"workflow edit-task {wf.id} {workflow_task_id} "
+        f"--args-file {args_file}"
+    )
+    debug(cmd)
+    res = await invoke(cmd)
+    assert res.retcode == 0
+    assert res.data["task_list"][0]["args"] == custom_args
