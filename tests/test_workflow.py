9f4a00f61de0f699f29b0c25bc64e6ae36611214
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-11-10 10:39:23 +0100

    Fix test_workflow (ref #314)

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -23,16 +23,20 @@ async def test_workflow_list(clear_db, testserver, register_user, invoke):
     PROJECT_NAME = "project_name"
     PROJECT_PATH = "project_path"
     res_pj = await invoke(f"project new {PROJECT_NAME} {PROJECT_PATH}")
+    project_id = res_pj.data["id"]
+    debug(project_id)
 
-    res_wf = await invoke(f"workflow new WF1 {res_pj.data['id']}")
+    res_wf = await invoke(f"workflow new WF1 {project_id}")
     res_wf.show()
     assert res_wf.retcode == 0
 
-    res_wf = await invoke(f"workflow new WF2 {res_pj.data['id']}")
+    res_wf = await invoke(f"workflow new WF2 {project_id}")
     res_wf.show()
     assert res_wf.retcode == 0
 
-    res_list = await invoke("workflow list")
+    res_list = await invoke(f"workflow list {project_id}")
+    debug(res_list)
+    debug(res_list.data)
     assert res_list.retcode == 0
     assert len(res_list.data) == 2
 
