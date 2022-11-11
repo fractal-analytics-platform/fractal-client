1d8b34a82bc0999f259ecb38e541d159facce33c
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-11-11 15:04:17 +0100

    add arbitrary data table
    close #254

--- fractal_server/app/models/__init__.py
+++ fractal_server/app/models/__init__.py
@@ -2,6 +2,7 @@ from ..schemas import *  # noqa F401
 from .project import *  # noqa: F403, F401
 from .run import *  # noqa: F403, F401
 from .security import *  # noqa: F403, F401
+from .state import State  # noqa: F401
 from .task import *  # noqa: F403, F401
 from .workflow import *  # noqa: F401, F403
 
