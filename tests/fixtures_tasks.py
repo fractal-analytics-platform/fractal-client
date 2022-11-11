620781baf895cb10867d5c75f6ec38acea613c21
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-11-11 13:50:53 +0100

    Add fixture dummy_task_package_invalid_manifest

--- tests/fixtures_tasks.py
+++ tests/fixtures_tasks.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 from pathlib import Path
 from typing import Any
 from typing import Dict
@@ -78,6 +79,42 @@ async def dummy_task_package(testdata_path, tmp_path) -> Path:
     await execute_command(f"cp -r {PACKAGE_TEMPLATE_PATH} {PACKAGE_PATH}")
     # copy content of task_package to PACKAGE_PATH
     await execute_command(f"cp {DUMMY_PACKAGE}/*.* {SOURCE_PATH}")
+    await execute_command("poetry build", cwd=PACKAGE_PATH)
+    wheel_relative = await execute_command("ls dist/*.whl", cwd=PACKAGE_PATH)
+    wheel_path = PACKAGE_PATH / wheel_relative
+    yield wheel_path
+
+
+@pytest.fixture
+async def dummy_task_package_invalid_manifest(testdata_path, tmp_path) -> Path:
+    """
+    Yields
+    ------
+    wheel_path : Path
+        the path to the built wheel package
+    """
+    from fractal_server import tasks as task_package
+
+    PACKAGE_TEMPLATE_PATH = testdata_path / "fractal-tasks-dummy"
+    PACKAGE_PATH = tmp_path / "fractal-tasks-dummy"
+    SOURCE_PATH = PACKAGE_PATH / "fractal_tasks_dummy"
+    DUMMY_PACKAGE = Path(task_package.__file__).parent
+
+    # copy template to temp
+    await execute_command(f"cp -r {PACKAGE_TEMPLATE_PATH} {PACKAGE_PATH}")
+    # copy content of task_package to PACKAGE_PATH
+    await execute_command(f"cp {DUMMY_PACKAGE}/*.* {SOURCE_PATH}")
+
+    # Make manifest invalid
+    MANIFEST_PATH = f"{SOURCE_PATH}/__FRACTAL_MANIFEST__.json"
+    with open(MANIFEST_PATH, "r") as fin:
+        manifest = json.load(fin)
+    task_list = manifest["task_list"]
+    task_list[0].pop("default_args")
+    manifest["task_list"] = task_list
+    with open(MANIFEST_PATH, "w") as fout:
+        json.dump(manifest, fout)
+
     await execute_command("poetry build", cwd=PACKAGE_PATH)
     wheel_relative = await execute_command("ls dist/*.whl", cwd=PACKAGE_PATH)
     wheel_path = PACKAGE_PATH / wheel_relative
