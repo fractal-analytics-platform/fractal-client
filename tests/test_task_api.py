41b1bfcb42237342fdf0e91af260325df1908ed6
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-11-15 15:01:20 +0100

    chagne name of user in test
    so that the package is installed in a different directory

--- tests/test_task_api.py
+++ tests/test_task_api.py
@@ -62,7 +62,7 @@ async def test_background_collection(db, dummy_task_package):
 async def test_background_collection_failure(db, dummy_task_package):
     task_pkg = _TaskCollectPip(package=dummy_task_package.as_posix())
     venv_path = create_package_dir_pip(
-        task_pkg=task_pkg, user="test_bg_collection"
+        task_pkg=task_pkg, user="test_bg_collection_fail"
     )
     collection_status = TaskCollectStatus(
         status="pending", venv_path=venv_path, package=task_pkg.package
