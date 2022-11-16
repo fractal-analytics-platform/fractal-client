7bc7e82f4a624cb846fe8daf755c4740ab7ee1cc
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-11-16 12:00:43 +0100

    Allow list of valid response status codes

--- fractal/client/response.py
+++ fractal/client/response.py
@@ -7,17 +7,24 @@ def check_response(res, expected_status_code=200, coerce=False):
     """
     Check the validity of the http response from fractal server
 
-    If the status code of the response is not the one expected, print the
-    error to stderr and terminate with exit status 1.
+    If the status code of the response is not one of the expected values, print
+    the error to stderr and terminate with exit status 1.
 
     On success, optionally coerce to a pydantic model
     """
+
+    # Also allow a list of expected status codes
+    if isinstance(expected_status_code, list):
+        expected_status_codes = expected_status_code
+    else:
+        expected_status_codes = [expected_status_code]
+
     logging.debug(res.status_code)
     try:
         data = res.json()
     except JSONDecodeError:
         data = {}
-    if res.status_code != expected_status_code:
+    if res.status_code not in expected_status_codes:
         logging.error(f"Server returned {res.status_code}")
         logging.error(
             f"Original request: {res._request.method} {res._request.url}"
