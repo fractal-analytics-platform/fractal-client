674407496b1a014afee6a57bfc18cbd9e048570e
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-11-16 12:54:35 +0100

    BROKEN Make server fixtures function-scoped

--- tests/fixtures_testserver.py
+++ tests/fixtures_testserver.py
@@ -3,50 +3,24 @@ import logging
 from os import environ
 
 import pytest
-
-
-@pytest.fixture(scope="session")
-def temp_data_dir(tmp_path_factory):
-    yield tmp_path_factory.mktemp("data")
-
-
-@pytest.fixture(scope="session")
-def temp_db_path(tmp_path_factory):
-    db_dir = tmp_path_factory.mktemp("db")
-    yield db_dir / "test.db"
+from devtools import debug
 
 
 @pytest.fixture(scope="function")
-def clear_db():
-    """
-    Clear the db without dropping the schema
-
-    ref: https://stackoverflow.com/a/5003705/283972
-    """
-    import contextlib
-    from fractal_server.app.db import DB
-    from sqlmodel import SQLModel
-
-    meta = SQLModel.metadata
-    with contextlib.closing(DB.engine_sync().connect()) as conn:
-        trans = conn.begin()
-        for table in reversed(meta.sorted_tables):
-            conn.execute(table.delete())
-        trans.commit()
-
-
-@pytest.fixture(scope="session")
-async def testserver(temp_data_dir, temp_db_path):
+async def testserver(tmp_path):
     # cf. https://stackoverflow.com/a/57816608/283972
     environ["JWT_SECRET_KEY"] = "secret_key"
     environ["DEPLOYMENT_TYPE"] = "development"
-    environ["DATA_DIR_ROOT"] = temp_data_dir.as_posix()
-    environ["FRACTAL_ROOT"] = temp_data_dir.as_posix()
+    environ["DATA_DIR_ROOT"] = tmp_path.as_posix()
+    environ["FRACTAL_ROOT"] = tmp_path.as_posix()
     environ["FRACTAL_LOGGING_LEVEL"] = str(logging.DEBUG)
 
     environ["DB_ENGINE"] = "sqlite"
 
-    tmp_db_path = temp_db_path
+    tmp_db_path = tmp_path / "db/test.db"
+    tmp_db_path.parent.mkdir()
+    debug(tmp_db_path)
+
     environ["SQLITE_PATH"] = tmp_db_path.as_posix()
 
     import uvicorn
@@ -54,10 +28,13 @@ async def testserver(temp_data_dir, temp_db_path):
     from multiprocessing import Process
 
     # INIT DB
+    # FIXME: this is probably not re-loading settings
     from fractal_server.app.db import DB
     from sqlmodel import SQLModel
 
     # import fractal_server.app.models  # noqa: F401
+
+    debug(dir(DB.engine_sync()))
 
     SQLModel.metadata.create_all(DB.engine_sync())
 
