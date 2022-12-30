cf9c78a5ee688e750702b24783a94bca7adcb4a0
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-12-30 10:58:33 +0100

    Improve test_add_delete_resource

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,13 +1,16 @@
 import pytest
 
 
-async def test_add_resource(register_user, invoke):
+async def test_add_delete_resource(register_user, invoke):
+
+    # Create a project with its default dataset
     res = await invoke("project new prj0 prj_path0")
     project_id = res.data["id"]
     dataset_id = res.data["dataset_list"][0]["id"]
     assert res.data["dataset_list"][0]["resource_list"] == []
 
-    PATH = "/new/ds/path"
+    # Add a resource
+    PATH = "/some/path"
     res = await invoke(
         f"dataset add-resource {project_id} {dataset_id} {PATH}"
     )
@@ -15,6 +18,26 @@ async def test_add_resource(register_user, invoke):
     assert res.retcode == 0
     assert res.data["path"] == PATH
     assert res.data["dataset_id"] == dataset_id
+    resource_id = res.data["id"]
+
+    # Remove a resource
+    res = await invoke(
+        f"dataset rm-resource {project_id} {dataset_id} {resource_id}"
+    )
+    assert res.retcode == 0
+
+    # Check that the resource was removed
+    res = await invoke(f"dataset show {project_id} {dataset_id}")
+    res.show()
+
+    # Add a new resource, and check that it has the same id as the one that was
+    # removed
+    res = await invoke(
+        f"dataset add-resource {project_id} {dataset_id} {PATH}"
+    )
+    res.show()
+    assert res.retcode == 0
+    assert resource_id == res.data["id"]
 
 
 async def test_add_resource_relative_path(register_user, invoke):
