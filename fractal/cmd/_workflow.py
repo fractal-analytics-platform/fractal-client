1f1a0440af1b939e75099c0a06ca62e32b0ce2f2
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-01-11 15:55:46 +0100

    Remove obsolete workflow_query_job_status function (close #401)

--- fractal/cmd/_workflow.py
+++ fractal/cmd/_workflow.py
@@ -20,35 +20,6 @@ from ..response import check_response
 from .utils import get_cached_task_by_name
 
 
-async def workflow_query_job_status(
-    client: AuthClient,
-    job_id: int,
-    batch: bool = False,
-    do_not_separate_logs: bool = False,
-    **kwargs,
-) -> BaseInterface:
-    """
-    Query the status of a workflow job
-    """
-
-    res = await client.get(f"{settings.BASE_URL}/job/{job_id}")
-    job = check_response(
-        res, expected_status_code=200, coerce=ApplyWorkflowRead
-    )
-    if batch:
-        return PrintInterface(retcode=0, data=job.status)
-    else:
-        data = job.sanitised_dict()
-        if do_not_separate_logs or (job.log is None):
-            return RichJsonInterface(retcode=0, data=data)
-        else:
-            log = data.pop("log")
-            extra_lines = ["\nThis is the job log:\n", log]
-            return RichJsonInterface(
-                retcode=0, data=data, extra_lines=extra_lines
-            )
-
-
 async def workflow_new(
     client: AuthClient,
     name: str,
