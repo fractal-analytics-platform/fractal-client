5311ae22279a0f23ae2e6f54ec7d0e00d6ab8c0e
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-01-12 16:13:26 +0100

    In test_auth_registered, also test with different `slurm_user`s

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,6 +1,7 @@
 from os import environ
 
 import pytest
+from devtools import debug
 
 from fractal.authclient import AuthenticationError
 from fractal.authclient import AuthToken
@@ -22,17 +23,22 @@ async def test_auth_fail(client):
         await auth()
 
 
-async def test_auth_registered(client, register_user):
+@pytest.mark.parametrize("slurm_user", [None, "env", "some_other_slurm_user"])
+async def test_auth_registered(client, register_user, slurm_user):
     """
-    GIVEN no user registered
-    WHEN when fetching a token
-    THEN authentication error is raised
+    GIVEN a registered user
+    WHEN when fetching a token (either with/without providing the slurm_user)
+    THEN token is obtained
     """
+    if slurm_user == "env":
+        slurm_user = environ.get("SLURM_USER")
+    debug(slurm_user)
     auth = AuthToken(
         client,
         username=environ.get("FRACTAL_USER"),
         password=environ.get("FRACTAL_PASSWORD"),
-        slurm_user=environ.get("SLURM_USER"),
+        slurm_user=slurm_user,
     )
     token = await auth()
     assert token
+    debug(token)
