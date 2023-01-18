1c35738829330b7d3702144c7902fdc62e7bf3a2
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-01-18 09:43:39 +0100

    Check logs in test_list

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,4 +1,5 @@
 from os import environ
+
 import pytest
 from devtools import debug
 
@@ -18,21 +19,23 @@ async def test_whoami_superuser(invoke_as_superuser):
     assert res.data["email"] == "admin@fractal.xy"
     assert res.data["is_superuser"]
 
-async def test_list(register_user, invoke):
-    with pytest.raises(SystemExit) as e:
-        res = await invoke("user list")
+
+async def test_list(register_user, invoke, caplog):
+    with pytest.raises(SystemExit):
+        await invoke("user list")
+    debug(caplog.text)
+    assert "403" in caplog.text
+    assert "Forbidden" in caplog.text
+
 
 async def test_list_superuser(invoke_as_superuser):
     res = await invoke_as_superuser("user list")
     assert res.retcode == 0
     debug(res.data)
 
+
 async def test_show_superuser(invoke_as_superuser):
-    res = await invoke_as_superuser(
-        f"user register a@b.c abc FK"
-    )
+    res = await invoke_as_superuser("user register a@b.c abc FK")
     debug(res, res.data)
-    res = await invoke_as_superuser(
-        f"user register c@b.a cba AE"
-    )
+    res = await invoke_as_superuser("user register c@b.a cba AE")
     debug(res, res.data)
