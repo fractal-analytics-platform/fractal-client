d9d988b132cca06f9ba76a9c796bb248bff66da7
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-02-13 08:40:36 +0100

    bump version 1.0.4 -> 1.0.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@ copyright = (
     "2022, Friedrich Miescher Institute for Biomedical Research and "
     "University of Zurich"
 )
-version = "1.0.4"
+version = "1.0.5"
 language = "en"
 
 # General configuration
