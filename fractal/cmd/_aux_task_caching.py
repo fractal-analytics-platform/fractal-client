3858e12f7781e75201f543607f13c2d0412681e1
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-05-26 12:41:46 +0200

    Add iternal function `_get_task_id` and its first unit tests (ref #345)

--- fractal/cmd/_aux_task_caching.py
+++ fractal/cmd/_aux_task_caching.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 from typing import Any
+from typing import Optional
 
 from ..authclient import AuthClient
 from ..config import settings
@@ -57,7 +58,47 @@ async def refresh_task_cache(client: AuthClient) -> list[dict[str, Any]]:
     return task_list
 
 
+def _get_task_id(
+    task_list: list[dict],
+    *,
+    name: Optional[str] = None,
+    _id: Optional[int] = None,
+    version: Optional[str] = "___VERSION_DEFAULT___",
+    owner: Optional[str] = "___OWNER_DEFAULT___",
+):
+    """
+    Given a task list, extract the ID of the single task matching some
+    conditions.
+
+    Note: this function also lets the user find tasks with `version` or `owner`
+    that are `None`; this won't be necessarily used in other public
+    functions.
+    """
+
+    def _condition(_task):
+        if name and _task["name"] != name:
+            return False
+        if version != "___VERSION_DEFAULT___" and _task["version"] != version:
+            return False
+        if owner != "___OWNER_DEFAULT___" and _task["owner"] != owner:
+            return False
+        if _id and _task["id"] != _id:
+            return False
+        return True
+
+    tasks = [_task for _task in task_list if _condition(_task)]
+    if len(tasks) == 0:
+        raise ValueError("No task matches required attributes")
+    elif len(tasks) > 1:
+        raise ValueError(
+            f"Multiple tasks ({len(tasks)}) match required attributes"
+        )
+    return tasks[0]["id"]
+
+
 async def get_cached_task_by_name(name: str, client: AuthClient) -> int:
+
+    """ """
 
     # Set paths
     cache_dir = Path(f"{settings.FRACTAL_CACHE_PATH}").expanduser()
