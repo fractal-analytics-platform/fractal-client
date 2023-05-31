e788491cbd5fd6fa18c5f4871a76c30fe13f1e65
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-05-31 16:57:01 +0200

    changed `test_get_task_id` into `test_get_matching_tasks`

--- tests/test_unit_task_cache.py
+++ tests/test_unit_task_cache.py
@@ -1,48 +1,33 @@
 import pytest
-from devtools import debug
 
 from fractal.cmd._aux_task_caching import _get_matching_tasks
 from fractal.cmd._aux_task_caching import _search_in_task_list
 from fractal.cmd._aux_task_caching import FractalCacheError
 
 
-def test_get_task_id(clear_task_cache):
-    TASK_LIST = [
-        # Common tasks (with owner=None)
-        dict(name="Task A", id=1, version="0.1.1", owner=None),
-        dict(name="Task A", id=2, version="1.2.0", owner=None),
-        dict(name="Task A", id=3, version="1.3.1", owner=None),
-        dict(name="Task B", id=4, version="0.1.1", owner=None),
-        dict(name="Task B", id=5, version="1.2.0", owner=None),
-        dict(name="Task B", id=6, version="1.3.1", owner=None),
-        # UserA tasks
-        dict(name="Task A", id=7, version="1.3.1", owner="User1"),
-    ]
+def test_get_matching_tasks(clear_task_cache):
 
-    NOTSET = "__THIS_VALUE_IS_NOT_SET__"
-    RAISE = "__THIS_MUST_FAIL__"
+    a = dict(name="bob", id=1, version="0.1.1")
+    b = dict(name="bob", id=2, version="1.2.0")
+    c = dict(name="bob", id=3, version="1.3.1")
+    d = dict(name="bar", id=4, version="0.0.0")
 
-    CASES = [
-        (NOTSET, NOTSET, 1, 1),
-        (NOTSET, NOTSET, NOTSET, 7),
-        (NOTSET, NOTSET, 999, RAISE),
-        # FIXME: add more cases
-    ]
-    for (name, version, _id, expected_id) in CASES:
-        kwargs = {}
-        if name != NOTSET:
-            kwargs["name"] = name
-        if version != NOTSET:
-            kwargs["version"] = version
-        if _id != NOTSET:
-            kwargs["_id"] = _id
-        debug(kwargs)
+    TASK_LIST = [a, b, c, d]
 
-        if expected_id == RAISE:
-            with pytest.raises(ValueError):
-                _get_matching_tasks(TASK_LIST, **kwargs)
-        else:
-            assert _get_matching_tasks(TASK_LIST, **kwargs) == expected_id
+    res = _get_matching_tasks(TASK_LIST, name="alice")
+    assert res == []
+
+    res = _get_matching_tasks(TASK_LIST, name="bob")
+    assert res == [a, b, c]
+
+    res = _get_matching_tasks(TASK_LIST, name="bob", version="1.2.0")
+    assert res == [b]
+
+    res = _get_matching_tasks(TASK_LIST, name="bar")
+    assert res == [d]
+
+    res = _get_matching_tasks(TASK_LIST, name="bar", version="3.1.4")
+    assert res == []
 
 
 async def test_search_in_task_list(clear_task_cache):
