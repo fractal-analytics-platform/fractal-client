19f0670f3ee3cfa57ea345924f798925812a410c
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-07-26 14:20:47 +0200

    Update docs/gen_ref_pages.py

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -4,12 +4,13 @@ from typing import Mapping
 
 import mkdocs_gen_files  # type: ignore[import]
 from mkdocs_gen_files import Nav
+from textwrap import fill
 
 from fractal.parser import parser_main
 import os
 from typing import Any, Optional
 
-from devtools import debug
+from devtools import debug  # type: ignore[import]
 
 import sys
 from pathlib import Path
@@ -29,7 +30,7 @@ def to_markdown(
     """
     name = data["name"]
     description = data["description"]
-    usage = data["usage"]
+    usage = data["usage"].replace("gen_ref_pages.py", "fractal")  # FIXME: why is this needed??  # noqa
 
     # Create MarkDown string for title
     if parent_cmd:
@@ -41,6 +42,12 @@ def to_markdown(
     description_str = f"{description}\n"
 
     # Create MarkDown string for usage code block
+    usage = fill(
+            usage,
+            width=72,
+            initial_indent='',
+            subsequent_indent=(' ' * 16),
+            )
     usage_str = f"```\n{usage}\n```\n"
 
     # Combine strings together
@@ -61,26 +68,20 @@ main = parse_parser(parser_main)
 
 # Create output_dir
 output_dir = "reference"
-if not os.path.isdir(output_dir):
-    os.mkdir(output_dir)
-if not os.path.isdir(f"{output_dir}/fractal"):
-    os.mkdir(f"{output_dir}/fractal")
 
 # Parser level 0
-nav[["fractal"]] = f"index.md"
-with open(f"{output_dir}/fractal/index.md", "w") as f:
+nav[["fractal"]] = f"fractal/index.md"
+main["name"] = "fractal"
+with mkdocs_gen_files.open(f"reference/fractal/index.md", "w") as f:
     f.write(to_markdown(main, level=0))
 
 # Parser levels 1 and 2 (commands and subcommands)
 for child in main["children"]:
     # Level 1
     name = child["name"]
-    folder = f"{output_dir}/fractal/{name}"
-    if not os.path.isdir(folder):
-        os.mkdir(folder)
-    path = f"{folder}/index.md"
-    nav[["fractal", name]] = f"{name}/index.md"
-    with open(path, "w") as f:
+
+    nav[["fractal", name]] = f"fractal/{name}/index.md"
+    with mkdocs_gen_files.open(f"reference/fractal/{name}/index.md", "w") as f:
         f.write(to_markdown(child, level=0))
         if "children" not in child.keys():
             continue
@@ -88,7 +89,7 @@ for child in main["children"]:
         for grandchild in child["children"]:
             f.write(to_markdown(grandchild, level=1, parent_cmd=name))
 
-with mkdocs_gen_files.open("reference/fractal/SUMMARY.md", "w") as nav_file:
+with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
     debug(nav.build_literate_nav())
     nav_file.writelines(nav.build_literate_nav())
 
