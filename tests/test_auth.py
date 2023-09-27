b92e9ee5755e7f6d44cd00cc4e4a03cf4c908602
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-09-27 13:14:16 +0200

    change test order

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -6,21 +6,6 @@ from fractal_client.authclient import AuthenticationError
 from fractal_client.authclient import AuthToken
 
 
-async def test_auth_fail(client):
-    """
-    GIVEN no user registered
-    WHEN when fetching a token
-    THEN authentication error is raised
-    """
-    with pytest.raises(AuthenticationError):
-        auth = AuthToken(
-            client,
-            username=environ.get("FRACTAL_USER"),
-            password=environ.get("FRACTAL_PASSWORD"),
-        )
-        await auth()
-
-
 async def test_auth_registered(client, register_user):
     """
     GIVEN no user registered
@@ -34,3 +19,18 @@ async def test_auth_registered(client, register_user):
     )
     token = await auth()
     assert token
+
+
+async def test_auth_fail(client):
+    """
+    GIVEN no user registered
+    WHEN when fetching a token
+    THEN authentication error is raised
+    """
+    with pytest.raises(AuthenticationError):
+        auth = AuthToken(
+            client,
+            username=environ.get("FRACTAL_USER"),
+            password=environ.get("FRACTAL_PASSWORD"),
+        )
+        await auth()
