209fa2dd26d6dafb5067a57f51553c02a5fa380c
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-01-31 10:38:42 +0100

    pretty print

--- fractal_client/interface.py
+++ fractal_client/interface.py
@@ -1,3 +1,16 @@
+import json
+
+
+def pretty_print(data) -> None:
+    if isinstance(data, dict):
+        print(json.dumps(data, indent=4))
+    elif isinstance(data, list):
+        for datum in data:
+            pretty_print(datum)
+    else:
+        print(str(data))
+
+
 class Interface:
     def __init__(self, retcode: int, data=None, extra=None):
         self.retcode = retcode
@@ -5,9 +18,9 @@ class Interface:
         self.extra = extra
 
     def show(self):
-        print(str(self.data))
+        pretty_print(self.data)
         if self.extra is not None:
-            print(str(self.extra))
+            pretty_print(self.extra)
 
     def __repr__(self):
         return f"<Interface (retcode={self.retcode})>"
