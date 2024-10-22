6fd305b66cbdfa21b3399c7080fbd0e79ad1824a
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-10-22 19:55:05 +0200

    fix tests/test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,28 +3,29 @@ from devtools import debug
 
 from fractal_client.authclient import AuthenticationError
 from fractal_client.authclient import AuthToken
+from fractal_client.authclient import Client
 
 
-def test_auth_registered(client, tester):
+def test_auth_registered(tester):
     """
     GIVEN an existing user
     WHEN fetching a token
     THEN authentication goes through
     """
     auth = AuthToken(
-        client, username=tester["email"], password=tester["password"]
+        client=Client(), username=tester["email"], password=tester["password"]
     )
     token = auth()
     assert token
 
 
-def test_auth_fail(client):
+def test_auth_fail():
     """
     GIVEN no user registered
     WHEN fetching a token
     THEN authentication error is raised
     """
     with pytest.raises(AuthenticationError) as err:
-        auth = AuthToken(client, username="foo", password="bar")
+        auth = AuthToken(client=Client(), username="foo", password="bar")
         auth()
     debug(err.value)
