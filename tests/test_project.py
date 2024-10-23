39cf8ff62c46b81c8db5b41923d19e8c06c7ebdc
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-10-23 11:05:40 +0200

    fix test_project.py and test_user.py

--- tests/test_project.py
+++ tests/test_project.py
@@ -2,17 +2,17 @@ import pytest
 from devtools import debug
 
 
-def test_project_create(register_user, invoke):
-    PROJECT_NAME = "project_name"
+def test_project_create(invoke, new_name):
+    PROJECT_NAME = new_name()
     res = invoke(f"project new {PROJECT_NAME}")
     debug(res)
     assert res.data["name"] == PROJECT_NAME
 
 
-def test_project_delete(register_user, invoke):
+def test_project_delete(invoke, new_name):
 
     # Create project
-    res = invoke("project new MyProj1")
+    res = invoke(f"project new {new_name()}")
     res.show()
     project_id_1 = res.data["id"]
 
@@ -29,11 +29,11 @@ def test_project_delete(register_user, invoke):
         res = invoke(f"project show {project_id_1}")
 
 
-def test_project_create_batch(register_user, invoke):
+def test_project_create_batch(invoke, new_name):
     res = invoke("project list")
     initial_projects = res.data
 
-    res = invoke("--batch project new MyProj1")
+    res = invoke(f"--batch project new {new_name()}")
     debug(res)
     debug(res.data)
     project_id = res.data
@@ -43,41 +43,43 @@ def test_project_create_batch(register_user, invoke):
     assert any(project["id"] == project_id for project in res.data)
 
 
-def test_project_list(register_user, invoke):
+def test_project_list(invoke, new_name):
     res = invoke("project list")
-    debug(res)
-    assert len(res.data) == 0
+    initial_projects = len(res.data)
 
     res.show()
 
-    res = invoke("--batch project new proj0")
+    res = invoke(f"--batch project new {new_name()}")
     project0_id = res.data
-    res = invoke(f"--batch project add-dataset {project0_id} NAME /tmp")
-    res = invoke("--batch project new proj1")
+    res = invoke(
+        f"--batch project add-dataset {project0_id} {new_name()} /tmp"
+    )
+    res = invoke(f"--batch project new {new_name()}")
 
     res = invoke("project list")
     debug(res)
     res.show()
-    assert len(res.data) == 2
+    assert len(res.data) == initial_projects + 2
 
 
-@pytest.mark.parametrize("new_name", ["new_name", None])
-def test_edit_project(register_user, invoke, new_name):
-    name = "name"
+@pytest.mark.parametrize("patch_name", [True, False])
+def test_edit_project(invoke, new_name, patch_name: bool):
+    name = new_name()
     res = invoke(f"project new {name}")
     project = res.data
     project_id = project["id"]
 
     cmd = f"project edit {project_id}"
-    if new_name:
-        cmd += f" --new-name {new_name}"
+    if patch_name:
+        NEW_NAME = new_name()
+        cmd += f" --new-name {NEW_NAME}"
 
     res = invoke(cmd)
     debug(res)
 
     assert res.retcode == 0
     new_project = res.data
-    if new_name:
-        assert new_project["name"] == new_name
+    if patch_name:
+        assert new_project["name"] == NEW_NAME
     else:
         assert new_project["name"] == name
