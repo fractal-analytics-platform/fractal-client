a6c553a1113a64b23d6db192db7aa52f99650bb9
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-05-27 11:19:28 +0200

    Improve internal interface

--- fractal_client/client.py
+++ fractal_client/client.py
@@ -58,10 +58,11 @@ def _verify_authentication_branch(
             "  2. Through environment variables.\n"
             "  3. Through environment variables in a `.fractal.env` file.\n"
         )
-        sys.exit(msg)
+        raise ValueError(msg)
 
 
-def handle(cli_args: list[str] = sys.argv):
+def handle(cli_args: list[str] = sys.argv) -> Interface:
+
     args = parser_main.parse_args(cli_args[1:])
 
     # Set logging level
@@ -83,32 +84,48 @@ def handle(cli_args: list[str] = sys.argv):
     if args.cmd is not None:
         handler = getattr(cmd, args.cmd)
     else:
-        # no command provided. Print help and exit 1
         parser_main.print_help()
-        exit(1)
+        sys.exit(1)
 
     try:
         # Make a copy of vars(args), and remove cmd (which is not a relevant
         # argument for functions called with **kwargs)
         kwargs = vars(args).copy()
         kwargs.pop("cmd")
-        fractal_server = kwargs.pop("fractal_server", settings.FRACTAL_SERVER)
+        fractal_server = (
+            kwargs.pop("fractal_server") or settings.FRACTAL_SERVER
+        )
+        logging.debug(f"Fractal server URL: {fractal_server}")
 
         if args.cmd == "version":
             interface = handler(fractal_server, **kwargs)
         else:
-            # Extract (and remove) username/password for AuthClient from kwargs
-            username = kwargs.pop("user", settings.FRACTAL_USER)
-            password = kwargs.pop("password", settings.FRACTAL_PASSWORD)
-            token_path = kwargs.pop("token_path", settings.FRACTAL_TOKEN_PATH)
-            _verify_authentication_branch(
-                username=username,
-                password=password,
-                token_path=token_path,
+            # Extract (and remove) credentials-related variables from kwargs
+            username = kwargs.pop("user") or settings.FRACTAL_USER
+            password = kwargs.pop("password") or settings.FRACTAL_PASSWORD
+            token_path = (
+                kwargs.pop("token_path") or settings.FRACTAL_TOKEN_PATH
             )
+            try:
+                _verify_authentication_branch(
+                    username=username,
+                    password=password,
+                    token_path=token_path,
+                )
+            except Exception as e:
+                return Interface(data=str(e), retcode=1)
+            # Read token from file
             if token_path is not None:
-                with open(token_path) as f:
-                    token = f.read().strip("\n")
+                try:
+                    with open(token_path) as f:
+                        token = f.read().strip("\n")
+                except Exception as e:
+                    msg = (
+                        f"Could not read token from {token_path=}.\n"
+                        f"Original error:\n{str(e)}"
+                    )
+                    return Interface(data=msg, retcode=1)
+
             else:
                 token = None
 
@@ -139,7 +156,7 @@ def handle(cli_args: list[str] = sys.argv):
 def main():
     interface = handle()
     interface.show()
-    exit(interface.retcode)
+    sys.exit(interface.retcode)
 
 
 if __name__ == "__main__":
